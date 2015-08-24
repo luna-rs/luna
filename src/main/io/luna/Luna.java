228535e122a1c08aa350e98e6bad7c7bd718a995
lare96 <lare96@live.com>
2015-08-23 21:53:44 -0400

    Moved global state from World -> Luna.

--- src/main/io/luna/Luna.java
+++ src/main/io/luna/Luna.java
@@ -1,11 +1,15 @@
 package io.luna;
 
+import io.luna.game.GameService;
+import io.luna.game.model.World;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * A class that contains the function invoked when this application is started.
+ * A class that manages global state and contains the function invoked when this
+ * application is started.
  * 
  * @author lare96 <http://github.org/lare96>
  */
@@ -15,6 +19,16 @@ public final class Luna {
      * The logger that will print important information.
      */
     private static final Logger LOGGER = LogManager.getLogger(Luna.class);
+
+    /**
+     * The game service that will run game logic.
+     */
+    private static GameService service = new GameService();
+
+    /**
+     * The world that will manage {@link io.luna.game.model.Entity} types.
+     */
+    private static World world = new World();
 
     /**
      * A private constructor to discourage external instantiation.
@@ -36,5 +50,23 @@ public final class Luna {
             LOGGER.catching(Level.FATAL, e);
             System.exit(0);
         }
+    }
+
+    /**
+     * Gets the game service that runs game logic.
+     * 
+     * @return The game service.
+     */
+    public static GameService getService() {
+        return service;
+    }
+
+    /**
+     * Gets the world that will manage {@code Entity} types.
+     * 
+     * @return The world that manages entities.
+     */
+    public static World getWorld() {
+        return world;
     }
 }
--- src/main/io/luna/game/model/World.java
+++ src/main/io/luna/game/model/World.java
@@ -1,33 +1,9 @@
 package io.luna.game.model;
 
-import io.luna.game.GameService;
-
 /**
- * Manages global state as well as the various types in the
- * {@code io.luna.game.model} package and subpackages.
+ * Manages the various types in the {@code io.luna.game.model} package and
+ * subpackages.
  * 
  * @author lare96 <http://github.org/lare96>
  */
-public final class World {
-
-    // TODO: Manage global state in the Luna class?
-
-    /**
-     * The main game logic service.
-     */
-    private static GameService service = new GameService();
-
-    /**
-     * A private constructor to discourage external instantiation.
-     */
-    private World() {}
-
-    /**
-     * Gets the main game logic service.
-     * 
-     * @return the logic service.
-     */
-    public static GameService getService() {
-        return service;
-    }
-}
+public final class World {} // XXX: Placeholder.
