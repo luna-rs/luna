c2f7486857e3cfa46f1228d27e633d95e42a91e2
lare96 <lare96@live.com>
2015-10-25 15:02:34 -0400

    Removed maximum player and npc constants

--- src/main/io/luna/game/model/EntityConstants.java
+++ src/main/io/luna/game/model/EntityConstants.java
@@ -1,6 +1,5 @@
 package io.luna.game.model;
 
-import io.luna.game.model.mobile.Npc;
 import io.luna.game.model.mobile.Player;
 
 /**
@@ -19,16 +18,6 @@ public final class EntityConstants {
      * The maximum distance that a {@link Player} can view.
      */
     public static final int VIEWING_DISTANCE = 15;
-
-    /**
-     * The maximum amount of active {@link Player}s that the protocol supports.
-     */
-    public static final int MAXIMUM_PLAYERS = 2048;
-
-    /**
-     * The maximum amount of active {@link Npc}s that the protocol supports.
-     */
-    public static final int MAXIMUM_NPCS = 16384;
 
     /**
      * A private constructor to discourage external instantiation.
