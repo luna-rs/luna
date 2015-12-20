1b2e922425d7407fccb48cdc886de55b71b768b5
lare96 <lare96@live.com>
2015-12-20 12:29:32 -0500

    All written outbound game messages documented and completed.

--- src/main/io/luna/net/msg/out/SendLogoutMessage.java
+++ src/main/io/luna/net/msg/out/SendLogoutMessage.java
@@ -4,6 +4,11 @@ import io.luna.game.model.mobile.Player;
 import io.luna.net.codec.ByteMessage;
 import io.luna.net.msg.OutboundGameMessage;
 
+/**
+ * An {@link OutboundGameMessage} implementation that disposes the login session.
+ *
+ * @author lare96 <http://github.org/lare96>
+ */
 public final class SendLogoutMessage extends OutboundGameMessage {
 
     @Override
@@ -12,5 +17,4 @@ public final class SendLogoutMessage extends OutboundGameMessage {
         msg.message(109);
         return msg;
     }
-
 }
--- src/main/io/luna/net/msg/out/SendWidgetItemGroupMessage.java
+++ src/main/io/luna/net/msg/out/SendWidgetItemGroupMessage.java
@@ -7,13 +7,31 @@ import io.luna.net.codec.ByteOrder;
 import io.luna.net.codec.ByteTransform;
 import io.luna.net.msg.OutboundGameMessage;
 
-public class SendUpdateInterfaceItemsMessage extends OutboundGameMessage {
+/**
+ * An {@link OutboundGameMessage} implementation that displays a group of {@link Item}s on a widget.
+ *
+ * @author lare96 <http://github.org/lare96>
+ */
+public final class SendWidgetItemGroupMessage extends OutboundGameMessage {
 
-    private final int interfaceId;
+    /**
+     * The identifier of the widget that the items will de displayed on.
+     */
+    private final int id;
+
+    /**
+     * The {@link Item}s that will be displayed on the widget.
+     */
     private final Item[] items;
 
-    public SendUpdateInterfaceItemsMessage(int interfaceId, Item[] items) {
-        this.interfaceId = interfaceId;
+    /**
+     * Creates a new {@link SendWidgetItemGroupMessage}.
+     *
+     * @param id The identifier of the widget that the items will de displayed on.
+     * @param items The {@link Item}s that will be displayed on the widget.
+     */
+    public SendWidgetItemGroupMessage(int id, Item[] items) {
+        this.id = id;
         this.items = items;
     }
 
@@ -22,7 +40,7 @@ public class SendUpdateInterfaceItemsMessage extends OutboundGameMessage {
         ByteMessage msg = ByteMessage.create();
 
         msg.varShortMessage(53);
-        msg.putShort(interfaceId);
+        msg.putShort(id);
         msg.putShort(items.length);
 
         for (Item item : items) {
