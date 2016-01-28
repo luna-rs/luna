96e9f3ca0541cee47c6a4cc5be2f3d6a6b026f32
lare96 <lare96@live.com>
2016-01-28 16:59:28 -0500

    Rewritten definition objects to match files.

--- src/main/java/io/luna/game/model/def/ItemDefinition.java
+++ src/main/java/io/luna/game/model/def/ItemDefinition.java
@@ -1,7 +1,6 @@
 package io.luna.game.model.def;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.primitives.Ints;
+import com.google.common.collect.ImmutableSet;
 import io.luna.game.model.item.Item;
 
 /**
@@ -17,233 +16,207 @@ public final class ItemDefinition {
     public static final ItemDefinition[] DEFINITIONS = new ItemDefinition[7956];
 
     /**
-     * The identifier for this {@code ItemDefinition}.
+     * The identifier for the {@code Item}.
      */
     private final int id;
 
     /**
-     * The name of this {@code ItemDefinition}.
+     * The name of the {@code Item}.
      */
     private final String name;
 
     /**
-     * The description of this {@code ItemDefinition}.
+     * The description of the {@code Item}.
      */
     private final String examine;
 
     /**
-     * The equipment slot of this {@code ItemDefinition}.
-     */
-    private final int equipmentSlot;
-
-    /**
-     * The flag that determines if this {@code ItemDefinition} is noteable.
-     */
-    private final boolean noteable;
-
-    /**
-     * The flag that determines if this {@code ItemDefinition} is stackable.
+     * If the {@code Item} is stackable.
      */
     private final boolean stackable;
 
     /**
-     * The special store value of this {@code ItemDefinition}.
+     * The base value of the {@code Item}, used for shop prices and low/high alch values.
+     */
+    private final int baseValue;
+
+    /**
+     * The 'special' value of the {@code Item}, unrelated to the base value.
      */
     private final int specialValue;
 
     /**
-     * The general store value of this {@code ItemDefinition}.
+     * The noted id of the {@code Item}, -1 if this definition is noted.
      */
-    private final int generalValue;
+    private final int notedId;
 
     /**
-     * The low alchemy value of this {@code ItemDefinition}.
+     * The unnoted id of the {@code Item}, -1 if this definition is unnoted.
      */
-    private final int lowAlchValue;
+    private final int unnotedId;
 
     /**
-     * The high alchemy value of this {@code ItemDefinition}.
+     * If the {@code Item} is for members only.
      */
-    private final int highAlchValue;
+    private final boolean membersOnly;
 
     /**
-     * The weight value of this {@code ItemDefinition}.
+     * The weight value of the {@code Item}.
      */
     private final double weight;
 
     /**
-     * The {@link ImmutableList} of bonuses for this {@code ItemDefinition}.
+     * If the {@code Item} is tradable.
      */
-    private final ImmutableList<Integer> bonus;
+    private final boolean tradable;
 
     /**
-     * The flag that determines if this {@code ItemDefinition} is two-handed.
+     * The inventory actions of the {@code Item}.
      */
-    private final boolean twoHanded;
+    private final ImmutableSet<String> inventoryActions;
 
     /**
-     * The flag that determines if this {@code ItemDefinition} is a full helmet.
+     * The ground actions of the {@code Item}.
      */
-    private final boolean fullHelm;
-
-    /**
-     * The flag that determines if this {@code ItemDefinition} is a platebody.
-     */
-    private final boolean platebody;
-
-    /**
-     * The flag that determines if this {@code ItemDefinition} is tradeable.
-     */
-    private final boolean tradeable;
+    private final ImmutableSet<String> groundActions;
 
     /**
      * Creates a new {@link ItemDefinition}.
      *
-     * @param id The identifier for this {@code ItemDefinition}.
-     * @param name The name of this {@code ItemDefinition}.
-     * @param examine The description of this {@code ItemDefinition}.
-     * @param equipmentSlot The equipment slot of this {@code ItemDefinition}.
-     * @param noteable The flag that determines if this {@code ItemDefinition} is noteable.
-     * @param stackable The flag that determines if this {@code ItemDefinition} is stackable.
-     * @param specialValue The special store value of this {@code ItemDefinition}.
-     * @param generalValue The general store value of this {@code ItemDefinition}.
-     * @param lowAlchValue The low alchemy value of this {@code ItemDefinition}.
-     * @param highAlchValue The high alchemy value of this {@code ItemDefinition}.
-     * @param weight The weight value of this {@code ItemDefinition}.
-     * @param bonus The array of bonuses for this {@code ItemDefinition}.
-     * @param twoHanded The flag that determines if this {@code ItemDefinition} is two-handed.
-     * @param fullHelm The flag that determines if this {@code ItemDefinition} is a full helmet.
-     * @param platebody The flag that determines if this {@code ItemDefinition} is a platebody.
-     * @param tradeable The flag that determines if this {@code ItemDefinition} is tradeable.
+     * @param id The identifier for the {@code Item}.
+     * @param name The name of the {@code Item}.
+     * @param examine The description of the {@code Item}.
+     * @param stackable If the {@code Item} is stackable.
+     * @param baseValue The base value of the {@code Item}, used for shop prices and low/high alch values.
+     * @param specialValue The 'special' value of the {@code Item}, unrelated to the base value.
+     * @param notedId The noted id of the {@code Item}, -1 if this definition is noted.
+     * @param unnotedId The unnoted id of the {@code Item}, -1 if this definition is unnoted.
+     * @param membersOnly If the {@code Item} is for members only.
+     * @param weight The weight value of the {@code Item}.
+     * @param tradable If the {@code Item} is tradable.
+     * @param inventoryActions The inventory actions of the {@code Item}.
+     * @param groundActions The ground actions of the {@code Item}.
      */
-    public ItemDefinition(int id, String name, String examine, int equipmentSlot, boolean noteable, boolean stackable, int specialValue, int generalValue, int lowAlchValue, int highAlchValue, double weight, int[] bonus, boolean twoHanded, boolean fullHelm, boolean platebody, boolean tradeable) {
+    public ItemDefinition(int id, String name, String examine, boolean stackable, int baseValue, int specialValue,
+        int notedId, int unnotedId, boolean membersOnly, double weight, boolean tradable, String[] inventoryActions,
+        String[] groundActions) {
         this.id = id;
         this.name = name;
         this.examine = examine;
-        this.equipmentSlot = equipmentSlot;
-        this.noteable = noteable;
         this.stackable = stackable;
+        this.baseValue = baseValue;
         this.specialValue = specialValue;
-        this.generalValue = generalValue;
-        this.lowAlchValue = lowAlchValue;
-        this.highAlchValue = highAlchValue;
+        this.notedId = notedId;
+        this.unnotedId = unnotedId;
+        this.membersOnly = membersOnly;
         this.weight = weight;
-        this.bonus = ImmutableList.copyOf(Ints.asList(bonus));
-        this.twoHanded = twoHanded;
-        this.fullHelm = fullHelm;
-        this.platebody = platebody;
-        this.tradeable = tradeable;
+        this.tradable = tradable;
+        this.inventoryActions = ImmutableSet.copyOf(inventoryActions);
+        this.groundActions = ImmutableSet.copyOf(groundActions);
     }
 
     /**
-     * @return The identifier for {@code ItemDefinition}.
+     * @return {@code true} if {@code action} is contained by the backing set of inventory actions.
+     */
+    public boolean hasInventoryAction(String action) {
+        return inventoryActions.contains(action);
+    }
+
+    /**
+     * @return {@code true} if {@code action} is contained by the backing set of ground actions.
+     */
+    public boolean hasGroundAction(String action) {
+        return groundActions.contains(action);
+    }
+
+    /**
+     * @return The identifier for the {@code Item}.
      */
     public int getId() {
         return id;
     }
 
     /**
-     * @return The name of {@code ItemDefinition}.
+     * @return The name of the {@code Item}.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * @return The description of {@code ItemDefinition}.
+     * @return The description of the {@code Item}.
      */
     public String getExamine() {
         return examine;
     }
 
     /**
-     * @return The equipment slot of {@code ItemDefinition}.
-     */
-    public int getEquipmentSlot() {
-        return equipmentSlot;
-    }
-
-    /**
-     * @return {@code true} if {@code ItemDefinition} is noteable, {@code false} otherwise.
-     */
-    public boolean isNoteable() {
-        return noteable;
-    }
-
-    /**
-     * @return {@code true} if {@code ItemDefinition} is stackable, {@code false} otherwise.
+     * @return If the {@code Item} is stackable.
      */
     public boolean isStackable() {
         return stackable;
     }
 
     /**
-     * @return The special store value of {@code ItemDefinition}.
+     * @return The base value of the {@code Item}, used for shop prices and low/high alch values.
+     */
+    public int getBaseValue() {
+        return baseValue;
+    }
+
+    /**
+     * @return The 'special' value of the {@code Item}, unrelated to the base value.
      */
     public int getSpecialValue() {
         return specialValue;
     }
 
     /**
-     * @return The general store value of {@code ItemDefinition}.
+     * @return The noted id of the {@code Item}, -1 if this definition is noted.
      */
-    public int getGeneralValue() {
-        return generalValue;
+    public int getNotedId() {
+        return notedId;
     }
 
     /**
-     * @return The low alchemy value of {@code ItemDefinition}.
+     * @return The unnoted id of the {@code Item}, -1 if this definition is unnoted.
      */
-    public int getLowAlchValue() {
-        return lowAlchValue;
+    public int getUnnotedId() {
+        return unnotedId;
     }
 
     /**
-     * @return The high alchemy value of {@code ItemDefinition}.
+     * @return If the {@code Item} is for members only.
      */
-    public int getHighAlchValue() {
-        return highAlchValue;
+    public boolean isMembersOnly() {
+        return membersOnly;
     }
 
     /**
-     * @return The weight value of {@code ItemDefinition}.
+     * @return The weight value of the {@code Item}.
      */
     public double getWeight() {
         return weight;
     }
 
     /**
-     * @return The {@link ImmutableList} of bonuses for {@this {@code ItemDefinition}.
+     * @return If the {@code Item} is tradable.
      */
-    public ImmutableList<Integer> getBonus() {
-        return bonus;
+    public boolean isTradable() {
+        return tradable;
     }
 
     /**
-     * @return {@code true} if {@code ItemDefinition} is two-handed, {@code false} otherwise.
+     * @return The inventory actions of the {@code Item}.
      */
-    public boolean isTwoHanded() {
-        return twoHanded;
+    public ImmutableSet<String> getInventoryActions() {
+        return inventoryActions;
     }
 
     /**
-     * @return {@code true} if {@code ItemDefinition} is a full helmet, {@code false} otherwise.
+     * @return The ground actions of the {@code Item}.
      */
-    public boolean isFullHelm() {
-        return fullHelm;
-    }
-
-    /**
-     * @return {@code true} if {@code ItemDefinition} is a platebody, {@code false} otherwise.
-     */
-    public boolean isPlatebody() {
-        return platebody;
-    }
-
-    /**
-     * @return {@code true} if {@code ItemDefinition} is tradable, {@code false} otherwise.
-     */
-    public boolean isTradeable() {
-        return tradeable;
+    public ImmutableSet<String> getGroundActions() {
+        return groundActions;
     }
 }
--- src/main/java/io/luna/game/model/def/NpcDefinition.java
+++ src/main/java/io/luna/game/model/def/NpcDefinition.java
@@ -1,5 +1,7 @@
 package io.luna.game.model.def;
 
+import com.google.common.collect.ImmutableSet;
+import com.sun.org.apache.xpath.internal.operations.String;
 import io.luna.game.model.mobile.Npc;
 
 /**
@@ -30,89 +32,34 @@ public final class NpcDefinition {
     private final String description;
 
     /**
-     * The combat level of the {@code Npc}.
-     */
-    private final int combatLevel;
-
-    /**
      * The size of the {@code Npc}.
      */
     private final int size;
 
     /**
-     * Determines if the {@code Npc} can be attacked.
+     * The walking animation for the {@code Npc}.
      */
-    private final boolean attackable;
+    private final int walkAnimation;
 
     /**
-     * Determines if the {@code Npc} is aggressive.
+     * The walking-back animation for the {@code Npc}.
      */
-    private final boolean aggressive;
+    private final int walkBackAnimation;
 
     /**
-     * Determines if the {@code Npc} retreats.
+     * The walking-left animation for the {@code Npc}.
      */
-    private final boolean retreats;
+    private final int walkLeftAnimation;
 
     /**
-     * Determines if the {@code Npc} is poisonous.
+     * The walking-right for the {@code Npc}.
      */
-    private final boolean poisonous;
+    private final int walkRightAnimation;
 
     /**
-     * The time it takes for the {@code Npc} to respawn.
+     * The actions for the {@code Npc}.
      */
-    private final int respawnTime;
-
-    /**
-     * The max hit of the {@code Npc}.
-     */
-    private final int maxHit;
-
-    /**
-     * The maximum amount of hitpoints the {@code Npc} has.
-     */
-    private final int hitpoints;
-
-    /**
-     * The attack speed of the {@code Npc}.
-     */
-    private final int attackSpeed;
-
-    /**
-     * The attack animation of the {@code Npc}.
-     */
-    private final int attackAnimation;
-
-    /**
-     * The defence animation of the {@code Npc}.
-     */
-    private final int defenceAnimation;
-
-    /**
-     * The death animation of the {@code Npc}.
-     */
-    private final int deathAnimation;
-
-    /**
-     * The attack bonus of the {@code Npc}.
-     */
-    private final int attackBonus;
-
-    /**
-     * The melee defence bonus of the {@code Npc}.
-     */
-    private final int meleeDefence;
-
-    /**
-     * The ranged defence of the {@code Npc}.
-     */
-    private final int rangedDefence;
-
-    /**
-     * The magic defence of the {@code Npc}.
-     */
-    private final int magicDefence;
+    private final ImmutableSet<String> actions;
 
     /**
      * Creates a new {@link NpcDefinition}.
@@ -120,45 +67,31 @@ public final class NpcDefinition {
      * @param id The identification for the {@code Npc}.
      * @param name The name of the {@code Npc}.
      * @param description The description of the {@code Npc}.
-     * @param combatLevel The combat level of the {@code Npc}.
      * @param size The size of the {@code Npc}.
-     * @param attackable Determines if the {@code Npc} can be attacked.
-     * @param aggressive Determines if the {@code Npc} is aggressive.
-     * @param retreats Determines if the {@code Npc} retreats.
-     * @param poisonous Determines if the {@code Npc} is poisonous.
-     * @param respawnTime The time it takes for the {@code Npc} to respawn.
-     * @param maxHit The max hit of the {@code Npc}.
-     * @param hitpoints The maximum amount of hitpoints the {@code Npc} has.
-     * @param attackSpeed The attack speed of the {@code Npc}.
-     * @param attackAnimation The attack animation of the {@code Npc}.
-     * @param defenceAnimation The defence animation of the {@code Npc}.
-     * @param deathAnimation The death animation of the {@code Npc}.
-     * @param attackBonus The attack bonus of the {@code Npc}.
-     * @param meleeDefence The melee defence bonus of the {@code Npc}.
-     * @param rangedDefence The ranged defence of the {@code Npc}.
-     * @param magicDefence The magic defence of the {@code Npc}.
+     * @param walkAnimation The walking animation for the {@code Npc}.
+     * @param walkBackAnimation The walking-back animation for the {@code Npc}.
+     * @param walkLeftAnimation The walking-left animation for the {@code Npc}.
+     * @param walkRightAnimation The walking-right animation for the {@code Npc}.
+     * @param actions The actions for the {@code Npc}.
      */
-    public NpcDefinition(int id, String name, String description, int combatLevel, int size, boolean attackable, boolean aggressive, boolean retreats, boolean poisonous, int respawnTime, int maxHit, int hitpoints, int attackSpeed, int attackAnimation, int defenceAnimation, int deathAnimation, int attackBonus, int meleeDefence, int rangedDefence, int magicDefence) {
+    public NpcDefinition(int id, String name, String description, int size, int walkAnimation, int walkBackAnimation,
+        int walkLeftAnimation, int walkRightAnimation, String[] actions) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.combatLevel = combatLevel;
         this.size = size;
-        this.attackable = attackable;
-        this.aggressive = aggressive;
-        this.retreats = retreats;
-        this.poisonous = poisonous;
-        this.respawnTime = respawnTime;
-        this.maxHit = maxHit;
-        this.hitpoints = hitpoints;
-        this.attackSpeed = attackSpeed;
-        this.attackAnimation = attackAnimation;
-        this.defenceAnimation = defenceAnimation;
-        this.deathAnimation = deathAnimation;
-        this.attackBonus = attackBonus;
-        this.meleeDefence = meleeDefence;
-        this.rangedDefence = rangedDefence;
-        this.magicDefence = magicDefence;
+        this.walkAnimation = walkAnimation;
+        this.walkBackAnimation = walkBackAnimation;
+        this.walkLeftAnimation = walkLeftAnimation;
+        this.walkRightAnimation = walkRightAnimation;
+        this.actions = ImmutableSet.copyOf(actions);
+    }
+
+    /**
+     * @return {@code true} if {@code action} is contained by the backing set of actions.
+     */
+    public boolean hasAction(String action) {
+        return actions.contains(action);
     }
 
     /**
@@ -183,13 +116,6 @@ public final class NpcDefinition {
     }
 
     /**
-     * @return The description of the {@code Npc}.
-     */
-    public int getCombatLevel() {
-        return combatLevel;
-    }
-
-    /**
      * @return The size of the {@code Npc}.
      */
     public int getSize() {
@@ -197,107 +123,37 @@ public final class NpcDefinition {
     }
 
     /**
-     * @return {@code true} if the {@code Npc} can be attacked, {@code false} otherwise.
+     * @return The walking animation for the {@code Npc}.
      */
-    public boolean isAttackable() {
-        return attackable;
+    public int getWalkAnimation() {
+        return walkAnimation;
     }
 
     /**
-     * @return {@code true} if the {@code Npc} is aggressive, {@code false} otherwise.
+     * @return The walking-back animation for the {@code Npc}.
      */
-    public boolean isAggressive() {
-        return aggressive;
+    public int getWalkBackAnimation() {
+        return walkBackAnimation;
     }
 
     /**
-     * @return {@code true} if the {@code Npc} can retreat, {@code false} otherwise.
+     * @return The walking-left animation for the {@code Npc}.
      */
-    public boolean isRetreats() {
-        return retreats;
+    public int getWalkLeftAnimation() {
+        return walkLeftAnimation;
     }
 
     /**
-     * @return {@code true} if the {@code Npc} is poisonous, {@code false} otherwise.
+     * @return The walking-right animation for the {@code Npc}.
      */
-    public boolean isPoisonous() {
-        return poisonous;
+    public int getWalkRightAnimation() {
+        return walkRightAnimation;
     }
 
     /**
-     * @return The time it takes for the {@code Npc} to respawn.
+     * @return The actions for the {@code Npc}.
      */
-    public int getRespawnTime() {
-        return ((respawnTime - 1) <= 0 ? 1 : (respawnTime - 1));
-    }
-
-    /**
-     * @return The max hit of the {@code Npc}.
-     */
-    public int getMaxHit() {
-        return maxHit;
-    }
-
-    /**
-     * @return The maximum amount of hitpoints the {@code Npc} has.
-     */
-    public int getHitpoints() {
-        return hitpoints;
-    }
-
-    /**
-     * @return The maximum amount of hitpoints the {@code Npc} has.
-     */
-    public int getAttackSpeed() {
-        return attackSpeed;
-    }
-
-    /**
-     * @return The attack animation of the {@code Npc}.
-     */
-    public int getAttackAnimation() {
-        return attackAnimation;
-    }
-
-    /**
-     * @return The defence animation of the {@code Npc}.
-     */
-    public int getDefenceAnimation() {
-        return defenceAnimation;
-    }
-
-    /**
-     * @return The death animation of the {@code Npc}.
-     */
-    public int getDeathAnimation() {
-        return deathAnimation;
-    }
-
-    /**
-     * @return The attack bonus of the {@code Npc}.
-     */
-    public int getAttackBonus() {
-        return attackBonus;
-    }
-
-    /**
-     * @return The melee defence bonus of the {@code Npc}.
-     */
-    public int getMeleeDefence() {
-        return meleeDefence;
-    }
-
-    /**
-     * @return The ranged defence of the {@code Npc}.
-     */
-    public int getRangedDefence() {
-        return rangedDefence;
-    }
-
-    /**
-     * @return The magic defence of the {@code Npc}.
-     */
-    public int getMagicDefence() {
-        return magicDefence;
+    public ImmutableSet<String> getActions() {
+        return actions;
     }
 }
