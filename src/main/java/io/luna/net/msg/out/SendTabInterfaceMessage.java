4e498d3cb5da83568bff7e5a621e3a9b08938874
lare96 <lare96@live.com>
2016-03-08 20:51:56 -0500

    Minor documentation changes, new message encoders.

--- src/main/java/io/luna/net/msg/out/SendTabInterfaceMessage.java
+++ src/main/java/io/luna/net/msg/out/SendTabInterfaceMessage.java
@@ -6,15 +6,14 @@ import io.luna.net.codec.ByteTransform;
 import io.luna.net.msg.OutboundGameMessage;
 
 /**
- * An {@link OutboundGameMessage} that forces a certain tab on the gameframe open for a specific {@link Player}. Used for
- * things like tutorial island to force the {@code Player} to view a certain tab.
+ * An {@link OutboundGameMessage} implementation that displays an interface on a sidebar tab.
  *
  * @author lare96 <http://github.org/lare96>
  */
-public final class SendForceTabMessage extends OutboundGameMessage {
+public final class SendTabInterfaceMessage extends OutboundGameMessage {
 
     /**
-     * The identifier of the game tab to open.
+     * The identifier for the tab to send the interface on.
      * <p>
      * <table BORDER CELLPADDING=3 CELLSPACING=1> <tr> <td ALIGN=CENTER><em>Tab</em></td> <td
      * ALIGN=CENTER><em>Identifier</em></td> </tr> <tr> <td>Combat</td> <td>0</td> </tr> <tr> <td>Skills</td> <td>1</td>
@@ -23,21 +22,29 @@ public final class SendForceTabMessage extends OutboundGameMessage {
      * <tr> <td>Friends</td> <td>8</td> </tr> <tr> <td>Ignores</td> <td>9</td> </tr> <tr> <td>Logout</td> <td>10</td> </tr>
      * <tr> <td>Settings</td> <td>11</td> </tr> <td>Emotes</td> <td>12</td> </tr> <td>Music</td> <td>13</td> </tr> </table>
      */
-    private final int id;
+    private final int tabId;
 
     /**
-     * Creates a new {@link SendForceTabMessage}.
-     *
-     * @param id The identifier of the game tab to open.
+     * The identifier for the interface to send on the tab.
      */
-    public SendForceTabMessage(int id) {
-        this.id = id;
+    private final int interfaceId;
+
+    /**
+     * Creates a new {@link SendTabInterfaceMessage}.
+     *
+     * @param tabId The identifier for the tab to send the interface on.
+     * @param interfaceId The identifier for the interface to send on the tab.
+     */
+    public SendTabInterfaceMessage(int tabId, int interfaceId) {
+        this.tabId = tabId;
+        this.interfaceId = interfaceId;
     }
 
     @Override
     public ByteMessage writeMessage(Player player) {
-        ByteMessage msg = ByteMessage.message(106);
-        msg.put(id, ByteTransform.C);
+        ByteMessage msg = ByteMessage.message(71);
+        msg.putShort(interfaceId);
+        msg.put(tabId, ByteTransform.A);
         return msg;
     }
 }
