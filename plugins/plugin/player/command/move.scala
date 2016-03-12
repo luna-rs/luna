80f98c4e01d3d1182eea6caa01e594fd715ae1c9
lare96 <lare96@live.com>
2016-03-11 19:56:45 -0500

    Fix for "move" command.

--- plugins/plugin/player/command/move.scala
+++ plugins/plugin/player/command/move.scala
@@ -2,9 +2,11 @@ import io.luna.game.event.impl.CommandEvent
 import io.luna.game.model.Position
 
 >>@[CommandEvent]("move") { (msg, plr) =>
-  val x = msg.getArgs(0)
-  val y = msg.getArgs(1)
-  val z = if (msg.getArgs.length == 3) msg.getArgs(2) else plr.getPosition.getZ
+  val args = msg.getArgs
+
+  val x = args(0).toInt
+  val y = args(1).toInt
+  val z = if (args.length == 3) args(2).toInt else plr.getPosition.getZ
 
   plr.teleport(new Position(x, y, z));
 }
