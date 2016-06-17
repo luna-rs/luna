94196875c65e33e523ad15850fc6793b25a91eb8
lare96 <lare96@live.com>
2016-06-17 16:05:51 -0400

    Plugin for testing animations, graphics, objects, etc.

---
+++ plugins/player/command/testing_cmd.scala
@@ -0,0 +1,26 @@
+import io.luna.game.event.impl.CommandEvent
+import io.luna.game.model.mobile.{Animation, Graphic}
+
+>>@[CommandEvent]("music", RIGHTS_DEV) { (msg, plr) =>
+  plr.sendMusic(msg.getArgs()(0).toInt)
+}
+
+>>@[CommandEvent]("interface", RIGHTS_DEV) { (msg, plr) =>
+  plr.sendInterface(msg.getArgs()(0).toInt)
+}
+
+>>@[CommandEvent]("sound", RIGHTS_DEV) { (msg, plr) =>
+  plr.sendSound(msg.getArgs()(0).toInt, 0, 0)
+}
+
+>>@[CommandEvent]("object", RIGHTS_DEV) { (msg, plr) =>
+  // TODO: Once object placement system is done
+}
+
+>>@[CommandEvent]("graphic", RIGHTS_DEV) { (msg, plr) =>
+  plr.graphic(new Graphic(msg.getArgs()(0).toInt))
+}
+
+>>@[CommandEvent]("animation", RIGHTS_DEV) { (msg, plr) =>
+  plr.animation(new Animation(msg.getArgs()(0).toInt))
+}
