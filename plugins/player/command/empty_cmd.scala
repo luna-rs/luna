0cc6b83621a4cebc209aa52f4e7dce0abb7df6e1
lare96 <lare96@live.com>
2016-06-17 14:58:23 -0400

    Plugin for emptying bank, equipment, and inventory containers

---
+++ plugins/player/command/empty_cmd.scala
@@ -0,0 +1,27 @@
+import io.luna.game.event.impl.CommandEvent
+import io.luna.game.model.mobile.update.UpdateFlagHolder.UpdateFlag
+
+>>@[CommandEvent]("empty", RIGHTS_DEV) { (msg, plr) =>
+  plr.inventory.clear
+  plr.bank.clear
+  plr.equipment.clear
+
+  plr.flag(UpdateFlag.APPEARANCE)
+
+  plr.sendMessage("You have successfully emptied your inventory, bank, and equipment.")
+}
+
+>>@[CommandEvent]("empty_inventory", RIGHTS_DEV) { (msg, plr) =>
+  plr.inventory.clear
+  plr.sendMessage("You have successfully emptied your inventory.")
+}
+
+>>@[CommandEvent]("empty_bank", RIGHTS_DEV) { (msg, plr) =>
+  plr.bank.clear
+  plr.sendMessage("You have successfully emptied your bank.")
+}
+
+>>@[CommandEvent]("empty_equipment", RIGHTS_DEV) { (msg, plr) =>
+  plr.equipment.clear
+  plr.sendMessage("You have successfully emptied your equipment.")
+}
