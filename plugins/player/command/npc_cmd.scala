7bda5db8685e0796bad4b0331379dcfca07d3026
lare96 <lare96@live.com>
2016-06-17 16:04:37 -0400

    plr.getWorld -> world

--- plugins/player/command/npc_cmd.scala
+++ plugins/player/command/npc_cmd.scala
@@ -2,7 +2,7 @@ import io.luna.game.event.impl.CommandEvent
 
 >>@[CommandEvent]("npc", RIGHTS_DEV) { (msg, plr) =>
   val args = msg.getArgs
-  plr.getWorld.addNpc(args(0).toInt, plr.position)
+  world.addNpc(args(0).toInt, plr.position)
 }
 
 >>@[CommandEvent]("player_npc", RIGHTS_DEV) { (msg, plr) =>
