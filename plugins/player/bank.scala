fcc563e0edcc888a0c2fb19ba78058e1521afb88
lare96 <lare96@live.com>
2016-06-18 18:00:14 -0400

    Bank withdraw option plugin

---
+++ plugins/player/bank.scala
@@ -0,0 +1,6 @@
+import io.luna.game.event.impl.ButtonClickEvent
+
+>>@[ButtonClickEvent](5387) { (msg, plr) => plr.attr("withdraw_as_note", false) }
+
+>>@[ButtonClickEvent](5386) { (msg, plr) => plr.attr("withdraw_as_note", true) }
+
