ca5c59f3021211e8fd2409554c1c21d21da342e6
lare96 <lare96@live.com>
2016-06-21 18:06:51 -0400

    Inventory cleanup and testing

--- src/main/java/io/luna/game/model/item/Inventory.java
+++ src/main/java/io/luna/game/model/item/Inventory.java
@@ -12,14 +12,12 @@ public final class Inventory extends ItemContainer {
     /**
      * An {@link ItemContainerAdapter} implementation that listens for changes to the inventory.
      */
-    private static final class InventoryListener extends ItemContainerAdapter {
+    private final class InventoryListener extends ItemContainerAdapter {
 
         /**
          * Creates a new {@link InventoryListener}.
-         *
-         * @param player The {@link Player} this instance is dedicated to.
          */
-        public InventoryListener(Player player) {
+        public InventoryListener() {
             super(player);
         }
 
@@ -40,13 +38,19 @@ public final class Inventory extends ItemContainer {
     public static final int INVENTORY_DISPLAY_ID = 3214;
 
     /**
+     * The {@link Player} this instance is dedicated to.
+     */
+    private final Player player;
+
+    /**
      * Creates a new {@link Inventory}.
      *
      * @param player The {@link Player} this instance is dedicated to.
      */
     public Inventory(Player player) {
         super(28, StackPolicy.STANDARD);
+        this.player = player;
 
-        addListener(new InventoryListener(player));
+        addListener(new InventoryListener());
     }
 }
