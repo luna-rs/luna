f5b2d14b1f41fb00a67d5b0db240b173c3709b10
lare96 <lare96@live.com>
2016-06-23 18:12:59 -0400

    EquipItemMessageReader, for equipment

---
+++ src/main/java/io/luna/net/msg/in/EquipItemMessageReader.java
@@ -0,0 +1,37 @@
+package io.luna.net.msg.in;
+
+import io.luna.game.event.Event;
+import io.luna.game.model.item.Inventory;
+import io.luna.game.model.mobile.Player;
+import io.luna.net.codec.ByteTransform;
+import io.luna.net.msg.GameMessage;
+import io.luna.net.msg.InboundMessageReader;
+
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * An {@link InboundMessageReader} implementation that decodes data sent when a {@link Player} equips an item.
+ *
+ * @author lare96 <http://github.org/lare96>
+ */
+public class EquipItemMessageReader extends InboundMessageReader {
+
+    @Override
+    public Event read(Player player, GameMessage msg) throws Exception {
+        int itemId = msg.getPayload().getShort(false);
+        int index = msg.getPayload().getShort(false, ByteTransform.A);
+        int interfaceId = msg.getPayload().getShort(false, ByteTransform.A);
+
+        checkState(itemId > 0, "itemId <= 0");
+        checkState(index >= 0, "index < 0");
+        checkState(interfaceId > 0, "interfaceId <= 0");
+
+        Inventory inventory = player.getInventory();
+        if (inventory.computeIdForIndex(index).orElse(-1) != itemId) {
+            return null;
+        }
+
+        player.getEquipment().equip(index);
+        return null;
+    }
+}
