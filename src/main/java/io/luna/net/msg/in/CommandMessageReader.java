84e2c5e492f162e671191f21196b2880a7475f53
lare96 <lare96@live.com>
2016-06-23 18:11:51 -0400

    Send player a message when doing ::hotfix

--- src/main/java/io/luna/net/msg/in/CommandMessageReader.java
+++ src/main/java/io/luna/net/msg/in/CommandMessageReader.java
@@ -1,14 +1,13 @@
 package io.luna.net.msg.in;
 
-import io.luna.LunaContext;
-import io.luna.game.GameService;
 import io.luna.game.event.Event;
 import io.luna.game.event.impl.CommandEvent;
 import io.luna.game.model.mobile.Player;
-import io.luna.game.plugin.PluginBootstrap;
-import io.luna.game.plugin.PluginManager;
+import io.luna.game.model.mobile.PlayerRights;
+import io.luna.game.plugin.Hotfixer;
 import io.luna.net.msg.GameMessage;
 import io.luna.net.msg.InboundMessageReader;
+import io.luna.net.msg.out.GameChatboxMessageWriter;
 
 /**
  * An {@link InboundMessageReader} implementation that decodes data sent when a {@link Player} tries to activate a command.
@@ -22,25 +21,30 @@ public final class CommandMessageReader extends InboundMessageReader {
         String string = msg.getPayload().getString();
         string = string.toLowerCase();
         int index = string.indexOf(' ');
+
+        // Has to be done in Java because of classpath conflicts.
+        if (string.equals("hotfix") && player.getRights().equalOrGreater(PlayerRights.DEVELOPER)) {
+            initHotfix(player);
+            return null;
+        }
+
         if (index == -1) {
-            return new CommandEvent(string);
+            return new CommandEvent(string, player.getRights());
         }
 
         String name = string.substring(0, index);
         String[] args = string.substring(index + 1).split(" ");
+        return new CommandEvent(name, args, player.getRights());
+    }
 
-        // Has to be done in Java because of classpath conflicts.
-        if (name.equals("reloadplugins")) {
-            // TODO: implement some sort of additional cache that will allow for "hot fixes" without potentially interrupting
-            // gameplay. for now just clear plugin cache and do async reload of bootstrap
-            LunaContext ctx = player.getContext();
-            PluginManager plugins = ctx.getPlugins();
-            GameService service = ctx.getService();
-            plugins.clear();
-            service.execute(new PluginBootstrap(ctx));
-            return null;
-        }
+    /**
+     * Initiates a bootstrapping process in which all plugins will be 'reloaded' dynamically. Also popularly known as
+     * 'hotfixing'.
+     */
+    private void initHotfix(Player player) {
+        player.queue(new GameChatboxMessageWriter("Hotfix request received, initializing Hotfixer..."));
 
-        return new CommandEvent(name, args);
+        Hotfixer.newHotfixer(player.getContext()).
+            addListener(it -> player.queue(new GameChatboxMessageWriter("Hotfix successfully completed!"))).init();
     }
 }
