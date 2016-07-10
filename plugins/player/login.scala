a75a5e7e48f88f366092496ddd35bd66369d5c64
lare96 <lare96@live.com>
2016-07-10 03:38:14 -0400

    STARTER_EQUIPMENT value to login.scala

--- plugins/player/login.scala
+++ plugins/player/login.scala
@@ -1,24 +1,42 @@
+import java.time.format.DateTimeFormatter
+
 import io.luna.game.event.impl.LoginEvent
-import io.luna.game.model.item.Item
+import io.luna.game.model.item.{Equipment, Item}
+
+
+val DATE_FORMATTER = DateTimeFormatter.ofPattern("MMMM d, uuuu")
 
 val STARTER_ITEMS = Vector(
-  new Item(995, 10000),
-  new Item(1323),
-  new Item(556, 500),
-  new Item(555, 500),
-  new Item(554, 500),
-  new Item(557, 500),
-  new Item(558, 1000),
-  new Item(841),
-  new Item(882, 750)
+  new Item(995, 10000), // Coins
+  new Item(556, 250), // Air runes
+  new Item(555, 250), // Water runes
+  new Item(554, 250), // Fire runes
+  new Item(557, 250), // Earth runes
+  new Item(558, 500), // Mind runes
+  new Item(841) // Shortbow
 )
 
+val STARTER_EQUIPMENT = Vector(
+  (Equipment.HEAD, new Item(1153)), // Iron full helm
+  (Equipment.CHEST, new Item(1115)), // Iron platebody
+  (Equipment.LEGS, new Item(1067)), // Iron platelegs
+  (Equipment.WEAPON, new Item(1323)), // Iron scimitar
+  (Equipment.SHIELD, new Item(1191)), // Iron kiteshield
+  (Equipment.HANDS, new Item(1063)), // Leather vambraces
+  (Equipment.RING, new Item(2570)), // Ring of life
+  (Equipment.CAPE, new Item(1019)), // Black cape
+  (Equipment.AMMUNITION, new Item(882, 750)) // Bronze arrows
+)
+
+
 >>[LoginEvent] { (msg, plr) => // Give "starter package" if new player.
-  val inventory = plr.getInventory
+  val inventory = plr.inventory
+  val equipment = plr.equipment
 
   if (plr.attr("first_login")) {
     plr.sendMessage("This is your first login. Enjoy your starter package!")
     inventory.addAll(STARTER_ITEMS)
+    equipment.bulkOperation { () => STARTER_EQUIPMENT.foreach(it => equipment.set(it(0), it(1))) }
     plr.attr("first_login", false)
   }
 }
