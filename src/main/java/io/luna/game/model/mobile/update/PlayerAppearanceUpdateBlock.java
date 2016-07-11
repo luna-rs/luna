a9640e16fa9391bedc977c423ca544bff34522d5
lare96 <lare96@live.com>
2016-07-11 07:57:30 -0400

    Encoding of equipment values

--- src/main/java/io/luna/game/model/mobile/update/PlayerAppearanceUpdateBlock.java
+++ src/main/java/io/luna/game/model/mobile/update/PlayerAppearanceUpdateBlock.java
@@ -1,5 +1,6 @@
 package io.luna.game.model.mobile.update;
 
+import io.luna.game.model.item.Equipment;
 import io.luna.game.model.mobile.Player;
 import io.luna.game.model.mobile.PlayerAppearance;
 import io.luna.game.model.mobile.update.UpdateFlagHolder.UpdateFlag;
@@ -26,52 +27,85 @@ public final class PlayerAppearanceUpdateBlock extends PlayerUpdateBlock {
         PlayerAppearance appearance = mob.getAppearance();
 
         buf.put(appearance.get(PlayerAppearance.GENDER));
-        buf.put(-1); // TODO: head icon
-        buf.put(-1); // TODO: skull icon
+        buf.put(-1);
+        buf.put(-1);
 
         if (mob.getTransformId() != -1) {
             buf.putShort(-1);
             buf.putShort(mob.getTransformId());
         } else {
             encodeModelValues(buf, mob);
-            encodeModelColorValues(buf, mob);
-            encodeAnimationValues(buf, mob);
-
-            buf.putLong(mob.getUsernameHash());
-            buf.put(mob.getCombatLevel());
-            buf.putShort(0); // Skill level, used for Burthrope Games' Room iirc
-
-            msg.put(buf.getBuffer().writerIndex(), ByteTransform.C);
-            msg.putBytes(buf);
-
-            buf.release();
         }
+        encodeModelColorValues(buf, mob);
+        encodeAnimationValues(buf, mob);
+
+        buf.putLong(mob.getUsernameHash());
+        buf.put(mob.getCombatLevel());
+        buf.putShort(0); // Skill level, used for Burthrope Games' Room iirc
+
+        msg.put(buf.getBuffer().writerIndex(), ByteTransform.C);
+        msg.putBytes(buf);
+        buf.release();
     }
 
     /**
      * Encodes values related to the model of the {@link Player}.
      */
     private void encodeModelValues(ByteMessage buf, Player mob) {
-        // TODO: encode equipment values
+        Equipment equipment = mob.getEquipment();
         PlayerAppearance appearance = mob.getAppearance();
 
-        buf.put(0);
-        buf.put(0);
-        buf.put(0);
-        buf.put(0);
+        buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.HEAD).orElse(0)); // Helmet model.
+        buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.CAPE).orElse(0)); // Cape model.
+        buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.AMULET).orElse(0)); // Amulet model.
+        buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.WEAPON).orElse(0)); // Weapon model.
 
-        buf.putShort(0x100 + appearance.get(PlayerAppearance.CHEST));
-        buf.put(0);
-        buf.putShort(0x100 + appearance.get(PlayerAppearance.ARMS));
-        buf.putShort(0x100 + appearance.get(PlayerAppearance.LEGS));
-        buf.putShort(0x100 + appearance.get(PlayerAppearance.HEAD));
-        buf.putShort(0x100 + appearance.get(PlayerAppearance.HANDS));
-        buf.putShort(0x100 + appearance.get(PlayerAppearance.FEET));
-
-        if (appearance.isMale()) {
-            buf.putShort(0x100 + appearance.get(PlayerAppearance.BEARD));
+        if (equipment.indexOccupied(Equipment.CHEST)) { // Chest model.
+            buf.putShort(0x200 + equipment.get(Equipment.CHEST).getId());
         } else {
-            buf.putShort(0);
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.CHEST));
+        }
+
+        buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.SHIELD).orElse(0)); // Shield model.
+
+        boolean isFullBody = equipment.retrieve(Equipment.CHEST).map(it -> it.getEquipmentDef().isFullBody()).
+            orElse(false);
+        if (isFullBody) { // Arms model.
+            buf.put(0);
+        } else {
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.ARMS));
+        }
+
+        if (equipment.indexOccupied(Equipment.LEGS)) { // Legs model.
+            buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.LEGS).get());
+        } else {
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.LEGS));
+        }
+
+        boolean isFullHelmet = equipment.retrieve(Equipment.HEAD).map(it -> it.getEquipmentDef().isFullHelmet()).
+            orElse(false);
+        if (isFullHelmet) { // Head model.
+            buf.put(0);
+        } else {
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.HEAD));
+        }
+
+        if (equipment.indexOccupied(Equipment.HANDS)) { // Hands model.
+            buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.HANDS).get());
+        } else {
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.HANDS));
+        }
+
+        if (equipment.indexOccupied(Equipment.FEET)) { // Feet model.
+            buf.putShort(0x200 + equipment.computeIdForIndex(Equipment.FEET).get());
+        } else {
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.FEET));
+        }
+
+        if (appearance.isFemale() || isFullHelmet) { // Beard model.
+            buf.put(0);
+        } else {
+            buf.putShort(0x100 + appearance.get(PlayerAppearance.BEARD));
         }
     }
 
@@ -92,7 +126,6 @@ public final class PlayerAppearanceUpdateBlock extends PlayerUpdateBlock {
      * Encodes values related to the animations of the {@link Player}.
      */
     private void encodeAnimationValues(ByteMessage buf, Player mob) {
-        // TODO: Walking, standing, running, turning animations
         buf.putShort(0x328);
         buf.putShort(0x337);
         buf.putShort(0x333);
