25948fabda6023ef215b50d47836156d1b405a3c
lare96 <lare96@live.com>
2016-07-24 21:17:24 -0400

    New shortcut functions to GsonUtils

--- src/main/java/io/luna/util/GsonUtils.java
+++ src/main/java/io/luna/util/GsonUtils.java
@@ -5,6 +5,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * A static-utility class that contains functions for manipulating {@code Object}s related to {@link Gson}.
  *
@@ -19,14 +23,23 @@ public final class GsonUtils {
         .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
 
     /**
-     * Gets {@code element} as {@code clazz} type.
-     *
-     * @param element The element to get as {@code clazz}.
-     * @param clazz The new type of {@code element}.
-     * @param <T> The underlying type.
-     * @return The {@code element} as {@code T}.
+     * Shortcut to function {@link Gson#fromJson(JsonElement, Class)}.
      */
     public static <T> T getAsType(JsonElement element, Class<T> clazz) {
         return GSON.fromJson(element, clazz);
+    }
+
+    /**
+     * Shortcut to function {@link Gson#toJsonTree(Object)}.
+     */
+    public static JsonElement toJsonTree(Object obj) {
+        return GSON.toJsonTree(obj);
+    }
+
+    /**
+     * Shortcut to function {@link Gson#toJson(JsonElement, Appendable)}.
+     */
+    public static void writeJson(JsonElement element, File file) throws IOException {
+        GSON.toJson(element, new FileWriter(file));
     }
 }
