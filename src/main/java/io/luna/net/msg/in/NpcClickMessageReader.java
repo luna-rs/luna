3d3e1f2af67514e6560596a124de26ed012505b9
lare96 <lare96@live.com>
2016-07-30 13:56:06 -0400

    DistancedAction for npc index clicks

--- src/main/java/io/luna/net/msg/in/NpcClickMessageReader.java
+++ src/main/java/io/luna/net/msg/in/NpcClickMessageReader.java
@@ -1,6 +1,8 @@
 package io.luna.net.msg.in;
 
+import io.luna.game.action.DistancedAction;
 import io.luna.game.event.Event;
+import io.luna.game.event.impl.NpcClickEvent;
 import io.luna.game.event.impl.NpcClickEvent.NpcFifthClickEvent;
 import io.luna.game.event.impl.NpcClickEvent.NpcFirstClickEvent;
 import io.luna.game.event.impl.NpcClickEvent.NpcFourthClickEvent;
@@ -9,11 +11,14 @@ import io.luna.game.event.impl.NpcClickEvent.NpcThirdClickEvent;
 import io.luna.game.model.World;
 import io.luna.game.model.mobile.Npc;
 import io.luna.game.model.mobile.Player;
+import io.luna.game.plugin.PluginManager;
 import io.luna.net.codec.ByteMessage;
 import io.luna.net.codec.ByteOrder;
 import io.luna.net.codec.ByteTransform;
 import io.luna.net.msg.GameMessage;
 import io.luna.net.msg.MessageReader;
+
+import java.util.function.Function;
 
 import static com.google.common.base.Preconditions.checkState;
 
@@ -29,67 +34,81 @@ public final class NpcClickMessageReader extends MessageReader {
         int opcode = msg.getOpcode();
         switch (opcode) {
         case 155:
-            return firstIndex(player, msg.getPayload());
-        case 72:
-            return secondIndex(player, msg.getPayload());
+            firstIndex(player, msg.getPayload());
+            break;
         case 17:
-            return thirdIndex(player, msg.getPayload());
+            secondIndex(player, msg.getPayload());
+            break;
+        case 72:
+            thirdIndex(player, msg.getPayload());
+            break;
         case 21:
-            return fourthIndex(player, msg.getPayload());
+            fourthIndex(player, msg.getPayload());
+            break;
         case 18:
-            return fifthIndex(player, msg.getPayload());
+            fifthIndex(player, msg.getPayload());
+            break;
         }
         return null;
     }
 
     /**
-     * Retrieve the instance of the npc being clicked.
+     * Handles any npc click index.
      */
-    private Npc retrieveNpc(Player player, int index) {
+    private void handleClick(Player player, int index, Function<Npc, NpcClickEvent> evt) {
         World world = player.getWorld();
+        checkState(index >= 0 && index < world.getNpcs().capacity(), "index[" + index + "] out of range");
 
-        checkState(index >= 0 && index < world.getNpcs().capacity(), "index out of range");
-        return world.getNpcs().get(index);
+        Npc npc = world.getNpcs().get(index);
+        player.submitAction(new DistancedAction<Player>(player, npc.getPosition(), npc.size(), true) {
+            @Override
+            protected void execute() {
+                PluginManager plugins = player.getPlugins();
+                plugins.post(evt.apply(npc), player);
+
+                player.interact(npc);
+            }
+        });
     }
 
     /**
      * Click the first index of an npc.
      */
-    private Event firstIndex(Player player, ByteMessage msg) {
+    private void firstIndex(Player player, ByteMessage msg) {
         int index = msg.getShort(ByteOrder.LITTLE);
-        return new NpcFirstClickEvent(retrieveNpc(player, index));
+        handleClick(player, index, NpcFirstClickEvent::new);
     }
 
     /**
      * Click the second index of an npc.
      */
-    private Event secondIndex(Player player, ByteMessage msg) {
-        int index = msg.getShort(ByteTransform.A);
-        return new NpcSecondClickEvent(retrieveNpc(player, index));
+    private void secondIndex(Player player, ByteMessage msg) {
+        int index = msg.getShort(ByteTransform.A, ByteOrder.LITTLE);
+        handleClick(player, index, NpcSecondClickEvent::new);
     }
 
     /**
      * Click the third index of an npc.
      */
-    private Event thirdIndex(Player player, ByteMessage msg) {
-        int index = msg.getShort(ByteTransform.A, ByteOrder.LITTLE);
-        return new NpcThirdClickEvent(retrieveNpc(player, index));
+    private void thirdIndex(Player player, ByteMessage msg) {
+        int index = msg.getShort(ByteTransform.A);
+        handleClick(player, index, NpcThirdClickEvent::new);
     }
 
     /**
      * Click the fourth index of an npc.
      */
-    private Event fourthIndex(Player player, ByteMessage msg) {
+    private void fourthIndex(Player player, ByteMessage msg) {
         int index = msg.getShort(false);
-        return new NpcFourthClickEvent(retrieveNpc(player, index));
+        handleClick(player, index, NpcFourthClickEvent::new);
     }
 
     /**
      * Click the fifth index of an npc.
      */
-    private Event fifthIndex(Player player, ByteMessage msg) {
+    private void fifthIndex(Player player, ByteMessage msg) {
         int index = msg.getShort(false, ByteOrder.LITTLE);
-        return new NpcFifthClickEvent(retrieveNpc(player, index));
+        handleClick(player, index, NpcFifthClickEvent::new);
     }
 }
 
