4580c95f96f01db6a978624fca6d5486da1123ab
lare96 <lare96@live.com>
2016-08-01 02:24:35 -0400

    Unit test for Position

---
+++ src/test/java/io/luna/game/model/PositionTest.java
@@ -0,0 +1,43 @@
+package io.luna.game.model;
+
+import org.junit.Test;
+
+/**
+ * A test that ensures that functions within {@link Position} are functioning correctly.
+ *
+ * @author lare96 <http://github.org/lare96>
+ */
+public final class PositionTest {
+
+    /**
+     * Test invalid {@code x} values.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidX() {
+        new Position(-1, 1, 1);
+    }
+
+    /**
+     * Test invalid {@code y} values.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidY() {
+        new Position(1, -1, 1);
+    }
+
+    /**
+     * Test invalid upper bound {@code z} values.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidUpperZ() {
+        new Position(1, 1, 4);
+    }
+
+    /**
+     * Test invalid lower bound {@code z} values.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidLowerZ() {
+        new Position(1, 1, -1);
+    }
+}
