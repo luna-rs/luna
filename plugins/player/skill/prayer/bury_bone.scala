3180c261bfe7fbe8dd0ff5369dc1b471a5a421f7
lare96 <lare96@live.com>
2016-08-10 18:12:46 -0400

    Removed symbols from bury_bone.scala, table isn't complex enough to warrant it

--- plugins/player/skill/prayer/bury_bone.scala
+++ plugins/player/skill/prayer/bury_bone.scala
@@ -18,42 +18,26 @@ import io.luna.game.model.mobile.Skill.PRAYER
 import io.luna.game.model.mobile.{Animation, Player}
 
 
-/* Class representing bones in the 'BONE_TABLE'. */
-private case class Bone(id: Int, exp: Double)
-
-
 /* Bone bury animation. */
-private val BURY_ANIMATION = new Animation(827)
+private val ANIMATION = new Animation(827)
 
-/*
- A table of all the bones that can be buried.
-
- bone_symbol -> Bone
-*/
-private val BONE_TABLE = Map(
-  'bones -> Bone(526, 4.5),
-  'bat_bones -> Bone(530, 5.2),
-  'monkey_bones -> Bone(3179, 5.0),
-  'wolf_bones -> Bone(2859, 4.5),
-  'big_bones -> Bone(532, 15.0),
-  'babydragon_bones -> Bone(534, 30.0),
-  'dragon_bones -> Bone(536, 72.0)
+/* A table of all the bones that can be buried. */
+private val BONES = Map(
+  526 -> 4.5, // Bones
+  530 -> 5.2, // Bat bones
+  3179 -> 5.0, // Monkey bones
+  2859 -> 4.5, // Wolf bones
+  532 -> 15.0, // Big bones
+  534 -> 30.0, // Babydragon bones
+  536 -> 72.0 // Dragon bones
 )
 
-/*
- A different mapping of the 'BONE_TABLE' that maps bone identifiers to their data.
-
- id -> Bone
-*/
-private val ID_TO_BONE = BONE_TABLE.values.map(bone => bone.id -> bone).toMap
-
-
 /* Attempt to bury a bone, if we haven't recently just buried one. */
-private def buryBone(plr: Player, bone: Bone) = {
+private def buryBone(plr: Player, id: Int, exp: Double) = {
   if (plr.elapsedTime("last_bone_bury", 1200)) {
 
     plr.interruptAction()
-    plr.animation(BURY_ANIMATION)
+    plr.animation(ANIMATION)
 
     plr.skill(PRAYER).addExperience(bone.exp)
     plr.inventory.remove(new Item(bone.id))
@@ -68,8 +52,9 @@ private def buryBone(plr: Player, bone: Bone) = {
 
 /* If the item being clicked is a bone, attempt to bury it. */
 intercept[ItemFirstClickEvent] { (msg, plr) =>
-  ID_TO_BONE.get(msg.getId).foreach { it =>
-    buryBone(plr, it)
+  val boneId = msg.getId
+  BONES.get(boneId).foreach { experience =>
+    buryBone(plr, boneId, experience)
     msg.terminate
   }
 }
