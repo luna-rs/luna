dc85fdbea8fbb61a9cbc8917e34e8a3f510a53b9
lare96 <lare96@live.com>
2016-08-12 00:38:57 -0400

    Documentation for search_nest.scala

--- plugins/player/skill/woodcutting/search_nest.scala
+++ plugins/player/skill/woodcutting/search_nest.scala
@@ -1,38 +1,68 @@
+/*
+ A plugin related to the Woodcutting skill that adds functionality for searching bird's nests.
+
+ SUPPORTS:
+  -> Searching all bird nest types.
+  -> Selection of random items for ring and seed bird nests.
+
+ AUTHOR: lare96
+*/
+
 import io.luna.game.event.impl.ItemClickEvent.ItemFirstClickEvent
 import io.luna.game.model.item.Item
 import io.luna.game.model.mobile.Player
 
 
+/* An empty nest item instance. */
 private val EMPTY_NEST = new Item(5075)
+
+/* A Seq of identifiers for seeds that can be obtained from nests. */
 private val SEEDS = Seq(5312, 5283, 5284, 5285, 5286, 5313, 5314, 5288, 5287, 5315, 5289, 5316, 5290, 5317).map(new Item(_))
+
+/* A Seq of identifiers for rings that can be obtained from nests. */
 private val RINGS = Seq(1635, 1637, 1639, 1641, 1643).map(new Item(_))
+
+/* A red egg item instance. */
 private val RED_EGG = new Item(5076)
+
+/* A blue egg item instance. */
 private val BLUE_EGG = new Item(5077)
+
+/* A green egg item instance. */
 private val GREEN_EGG = new Item(5078)
+
+/*
+ A Map with mappings of nest identifiers to { () => Item } functions. We use functions so that 'randomElement' will generate
+ a new element on each invocation.
+*/
 private val NESTS = Map(
   5070 -> (() => RED_EGG), // Red egg
   5071 -> (() => GREEN_EGG), // Green egg
   5072 -> (() => BLUE_EGG), // Blue egg
-  5073 -> (() => SEEDS.randomElement), // Seed
-  5074 -> (() => RINGS.randomElement) // Ring
+  5073 -> (() => SEEDS.randomElement), // Generate random seed
+  5074 -> (() => RINGS.randomElement) // Generate random ring
 )
 
 
-private def searchNest(plr: Player, nestId: Int, itemFunc: () => Item) = {
+/* A function that allows the player to search a nest. */
+private def searchNest(plr: Player, nestId: Int, nestFunc: () => Item) = {
   val inventory = plr.inventory
 
   if (inventory.computeRemainingSize >= 1) {
+    val nestItem = nestFunc()
+
     inventory.remove(new Item(nestId))
     inventory.add(EMPTY_NEST)
-    inventory.add(itemFunc())
+    inventory.add(nestItem)
 
-    plr.sendMessage("You remove the contents from the bird's nest.")
+    plr.sendMessage(joinArticle("You take", nameOfItem(nestItem.getId), "out of the bird's nest."))
   } else {
     plr.sendMessage("You do not have enough space in your inventory.")
   }
 }
 
 
+/* Intercept an event that will call 'searchNest' if a nest was clicked. */
 intercept[ItemFirstClickEvent] { (msg, plr) =>
   val nestId = msg.getId
   NESTS.get(nestId).foreach { item =>
