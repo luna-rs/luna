c22fb05abdcba22e9894942cade3f10b80fc8c50
lare96 <lare96@live.com>
2016-08-12 12:10:44 -0400

    Named parameters for consume_potion.scala table

--- plugins/player/consume/consume_potion.scala
+++ plugins/player/consume/consume_potion.scala
@@ -2,7 +2,7 @@
  A plugin that adds functionality for drinking potions.
 
  SUPPORTS:
-  -> Drinking a variety of no.
+  -> Drinking a variety of potions.
   -> Multi-portion foods (pizzas, cakes, etc).
   -> Foods with different throttle rates (karambwans, pies, etc).
 
@@ -24,12 +24,12 @@ import scala.collection.mutable
 
 /* Class representing potions in the 'POTION_TABLE'. */
 private case class Potion(
-                           fourDose: Int,
-                           threeDose: Int,
-                           twoDose: Int,
-                           oneDose: Int,
-                           effect: Player => Unit
-                         )
+  fourDose: Int,
+  threeDose: Int,
+  twoDose: Int,
+  oneDose: Int,
+  effect: Player => Unit
+)
 
 
 /* Item instance for empty vials. */
@@ -44,27 +44,131 @@ private val ANIMATION = new Animation(829)
  potion_symbol -> Potion
 */
 private val POTION_TABLE = Map(
-  'zamorak_brew -> Potion(2450, 189, 191, 193, onZamorakBrew),
-  'saradomin_brew -> Potion(6685, 6687, 6689, 6691, onSaradominBrew),
-  'agility_potion -> Potion(3032, 3034, 3036, 3038, plr => onSkillPotion(plr, AGILITY)),
-  'fishing_potion -> Potion(2438, 151, 153, 155, plr => onSkillPotion(plr, FISHING)),
-  'ranging_potion -> Potion(2444, 169, 171, 173, plr => onCombatPotion(plr, RANGED)),
-  'magic_potion -> Potion(3040, 3042, 3044, 3046, plr => onCombatPotion(plr, MAGIC)),
-  'defence_potion -> Potion(2432, 133, 135, 137, plr => onCombatPotion(plr, DEFENCE)),
-  'strength_potion -> Potion(113, 115, 117, 119, plr => onCombatPotion(plr, STRENGTH)),
-  'attack_potion -> Potion(2428, 121, 123, 125, plr => onCombatPotion(plr, ATTACK)),
-  'super_defence -> Potion(2442, 163, 165, 167, plr => onCombatPotion(plr, DEFENCE, true)),
-  'super_attack -> Potion(2436, 145, 147, 149, plr => onCombatPotion(plr, ATTACK, true)),
-  'super_strength -> Potion(2440, 157, 159, 161, plr => onCombatPotion(plr, STRENGTH, true)),
-  'energy_potion -> Potion(3008, 3010, 3012, 3014, onEnergyPotion),
-  'super_energy -> Potion(3016, 3018, 3020, 3022, plr => onEnergyPotion(plr, true)),
-  'antipoison_potion -> Potion(2446, 175, 177, 179, onAntipoison),
-  'super_antipoison -> Potion(2448, 181, 183, 185, plr => onAntipoison(plr, true, 500)),
-  'antidote_+ -> Potion(5943, 5945, 5947, 5949, plr => onAntipoison(plr, true, 1000)),
-  'antidote_++ -> Potion(5952, 5954, 5956, 5958, plr => onAntipoison(plr, true, 1200)),
-  'prayer_potion -> Potion(2434, 139, 141, 143, onPrayerPotion),
-  'anti_fire_potion -> Potion(2452, 2454, 2456, 2458, onAntifirePotion),
-  'super_restore -> Potion(3024, 3026, 3028, 3030, plr => onRestorePotion(plr, true))
+  'zamorak_brew -> Potion(fourDose = 2450,
+    threeDose = 189,
+    twoDose = 191,
+    oneDose = 193,
+    effect = onZamorakBrew),
+
+  'saradomin_brew -> Potion(fourDose = 6685,
+    threeDose = 6687,
+    twoDose = 6689,
+    oneDose = 6691,
+    effect = onSaradominBrew),
+
+  'agility_potion -> Potion(fourDose = 3032,
+    threeDose = 3034,
+    twoDose = 3036,
+    oneDose = 3038,
+    effect = onSkillPotion(_, AGILITY)),
+
+  'fishing_potion -> Potion(fourDose = 2438,
+    threeDose = 151,
+    twoDose = 153,
+    oneDose = 155,
+    effect = onSkillPotion(_, FISHING)),
+
+  'ranging_potion -> Potion(fourDose = 2444,
+    threeDose = 169,
+    twoDose = 171,
+    oneDose = 173,
+    effect = onCombatPotion(_, RANGED)),
+
+  'magic_potion -> Potion(fourDose = 3040,
+    threeDose = 3042,
+    twoDose = 3044,
+    oneDose = 3046,
+    effect = onCombatPotion(_, MAGIC)),
+
+  'defence_potion -> Potion(fourDose = 2432,
+    threeDose = 133,
+    twoDose = 135,
+    oneDose = 137,
+    effect = onCombatPotion(_, DEFENCE)),
+
+  'strength_potion -> Potion(fourDose = 113,
+    threeDose = 115,
+    twoDose = 117,
+    oneDose = 119,
+    effect = onCombatPotion(_, STRENGTH)),
+
+  'attack_potion -> Potion(fourDose = 2428,
+    threeDose = 121,
+    twoDose = 123,
+    oneDose = 125,
+    effect = onCombatPotion(_, ATTACK)),
+
+  'super_defence -> Potion(fourDose = 2442,
+    threeDose = 163,
+    twoDose = 165,
+    oneDose = 167,
+    effect = onCombatPotion(_, DEFENCE, true)),
+
+  'super_attack -> Potion(fourDose = 2436,
+    threeDose = 145,
+    twoDose = 147,
+    oneDose = 149,
+    effect = onCombatPotion(_, ATTACK, true)),
+
+  'super_strength -> Potion(fourDose = 2440,
+    threeDose = 157,
+    twoDose = 159,
+    oneDose = 161,
+    effect = onCombatPotion(_, STRENGTH, true)),
+
+  'energy_potion -> Potion(fourDose = 3008,
+    threeDose = 3010,
+    twoDose = 3012,
+    oneDose = 3014,
+    effect = onEnergyPotion),
+
+  'super_energy -> Potion(fourDose = 3016,
+    threeDose = 3018,
+    twoDose = 3020,
+    oneDose = 3022,
+    effect = onEnergyPotion(_, true)),
+
+  'antipoison_potion -> Potion(fourDose = 2446,
+    threeDose = 175,
+    twoDose = 177,
+    oneDose = 179,
+    effect = onAntipoison),
+
+  'super_antipoison -> Potion(fourDose = 2448,
+    threeDose = 181,
+    twoDose = 183,
+    oneDose = 185,
+    effect = onAntipoison(_, true, 500)),
+
+  'antidote_+ -> Potion(fourDose = 5943,
+    threeDose = 5945,
+    twoDose = 5947,
+    oneDose = 5949,
+    effect = onAntipoison(_, true, 1000)),
+
+  'antidote_++ -> Potion(fourDose = 5952,
+    threeDose = 5954,
+    twoDose = 5956,
+    oneDose = 5958,
+    effect = onAntipoison(_, true, 1200)),
+
+  'prayer_potion -> Potion(fourDose = 2434,
+    threeDose = 139,
+    twoDose = 141,
+    oneDose = 143,
+    effect = onPrayerPotion),
+
+  'anti_fire_potion -> Potion(fourDose = 2452,
+    threeDose = 2454,
+    twoDose = 2456,
+    oneDose = 2458,
+    effect = onAntifirePotion),
+
+  'super_restore -> Potion(fourDose = 3024,
+    threeDose = 3026,
+    twoDose = 3028,
+    oneDose = 3030,
+    effect = onRestorePotion(_, true))
 )
 
 /*
@@ -187,7 +291,7 @@ private def consume(plr: Player, potion: Potion, index: Int): Unit = {
       inventory.add(VIAL)
     }
 
-    plr.sendMessage(s"You drink some of your ${computeItemName(toConsume.getId)}.")
+    plr.sendMessage(s"You drink some of your ${nameOfItem(toConsume.getId)}.")
     val dosesLeft = ids.length - nextIndex
     if (dosesLeft > 0) {
       plr.sendMessage(s"You have $dosesLeft doses of potion left.")
