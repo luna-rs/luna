e43aa4bf462afceb6166f5f4636c0fbbaf0f9f87
lare96 <lare96@live.com>
2016-08-13 02:16:25 -0400

    open_bank.scala now uses intercept_@

--- plugins/player/bank/open_bank.scala
+++ plugins/player/bank/open_bank.scala
@@ -14,13 +14,7 @@
 import io.luna.game.event.impl.ObjectClickEvent.ObjectFirstClickEvent
 
 
-/* A set of identifiers for bank objects. */
-private val BANKS = Set(3193, 2213, 3095)
-
-
 /* If the object clicked is a bank, open the banking interface. */
-intercept[ObjectFirstClickEvent] { (msg, plr) =>
-  if (BANKS.contains(msg.getId)) {
-    plr.bank.open
-  }
+intercept_@[ObjectFirstClickEvent](3193, 2213, 3095) { (msg, plr) =>
+  plr.bank.open
 }
