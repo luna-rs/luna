8da737445f01df33cb79367820cd5bd5cac2fbae
lare96 <lare96@live.com>
2017-08-17 01:40:43 -0400

    Removed 'Chance' class, put constants into 'Rational'

--- src/main/java/io/luna/util/Rational.java
+++ src/main/java/io/luna/util/Rational.java
@@ -13,6 +13,41 @@ import static com.google.common.base.Preconditions.checkArgument;
 public final class Rational extends Number {
 
     /**
+     * A rational number describing an {@code ALWAYS} chance (100%, 1 in 1 chance).
+     */
+    public static final Rational ALWAYS = new Rational(1, 1);
+
+    /**
+     * A rational number describing a {@code VERY_COMMON} chance (50%, 1 in 2 chance).
+     */
+    public static final Rational VERY_COMMON = new Rational(1, 2);
+
+    /**
+     * A rational number describing an {@code COMMON} chance (25%, 1 in 4 chance).
+     */
+    public static final Rational COMMON = new Rational(1, 4);
+
+    /**
+     * A rational number describing an {@code UNCOMMON} chance (10%, 1 in 10 chance).
+     */
+    public static final Rational UNCOMMON = new Rational(1, 10);
+
+    /**
+     * A rational number describing an {@code VERY_UNCOMMON} chance (2.5%, 5 in 200 chance).
+     */
+    public static final Rational VERY_UNCOMMON = new Rational(5, 200);
+
+    /**
+     * A rational number describing an {@code RARE} chance (0.7%, 1 in 150 chance).
+     */
+    public static final Rational RARE = new Rational(1, 150);
+
+    /**
+     * A rational number describing an {@code VERY_RARE} chance (0.3%, 1 in 300 chance).
+     */
+    public static final Rational VERY_RARE = new Rational(1, 300);
+
+    /**
      * The numerator.
      */
     private final int numerator;
@@ -25,7 +60,7 @@ public final class Rational extends Number {
     /**
      * Create a {@link Rational}.
      *
-     * @param numerator The numerator.
+     * @param numerator   The numerator.
      * @param denominator The denominator.
      */
     public Rational(int numerator, int denominator) {
