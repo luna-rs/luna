2ee47a4ea28b2e987d647a11d13181a2d86d1eef
lare96 <lare96@live.com>
2018-08-23 05:40:13 -0400

    Spacing modification

--- src/main/java/io/luna/net/msg/out/RegionChangeMessageWriter.java
+++ src/main/java/io/luna/net/msg/out/RegionChangeMessageWriter.java
@@ -15,6 +15,7 @@ public final class RegionChangeMessageWriter extends MessageWriter {
     @Override
     public ByteMessage write(Player player) {
         ByteMessage msg = ByteMessage.message(73);
+
         msg.putShort(player.getPosition().getRegionX() + 6, ByteTransform.A);
         msg.putShort(player.getPosition().getRegionY() + 6);
         return msg;
