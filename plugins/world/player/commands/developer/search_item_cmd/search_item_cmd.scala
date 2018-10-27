81740a373fde94f89d46ffdd1642dde3b8e6dcb0
lare96 <lare96@live.com>
2018-10-27 12:13:29 -0400

    Fixes to search_item command plugin

--- plugins/world/player/commands/developer/search_item_cmd/search_item_cmd.scala
+++ plugins/world/player/commands/developer/search_item_cmd/search_item_cmd.scala
@@ -9,15 +9,13 @@ import scala.collection.mutable.ArrayBuffer
 
 
 /* An interface representing the result of a search. */
-private final class SearchResultInterface(plr: Player, searchItem: String) extends StandardInterface(5292) {
+private final class SearchResultInterface(searchItem: String) extends StandardInterface(5292) {
 
-  /* A list of text widget messages. */
+  /* A list of empty text widget messages. */
   private val TEXT_WIDGETS = Set(5388, 5389, 5390, 5391, 8132, 8133).
     map(new WidgetTextMessageWriter("", _))
 
-  /* Perform search for items and display them on the interface. */
-  def init = {
-
+  override def onOpen(plr: Player) = {
     // Perform search here, filter selected items into displayBuffer.
     val displayBuffer = new ArrayBuffer[Item]()
     ItemDefinition.ALL.forEach { itemDef =>
@@ -29,31 +27,34 @@ private final class SearchResultInterface(plr: Player, searchItem: String) exten
       }
     }
 
-    // Truncate results if necessary.
-    val displaySize = displayBuffer.size
-    val cap = plr.getBank.capacity
-    if (displaySize > cap) {
-      plr.sendMessage("The search has been truncated because there are too many results to be displayed.")
-      displayBuffer.dropRight(displaySize - cap)
-    } else {
-      plr.sendMessage(s"There are $displaySize results for search term '$searchItem'.")
+    // Convert buffer into raw array, truncating results if necessary.
+    val displayArray = {
+      val displaySize = displayBuffer.size
+      val cap = plr.getBank.capacity
+      if (displaySize > cap) {
+        plr.sendMessage("The search has been truncated because there are too many results to be displayed.")
+        displayBuffer.dropRight(displaySize - cap).toArray
+      } else {
+        plr.sendMessage(s"Found $displaySize results for search term '$searchItem'.")
+        displayBuffer.toArray
+      }
     }
 
+
     // Send packets.
-    plr.queue(new WidgetTextMessageWriter(s"Search results for '$searchItem' ...", 5383))
+    plr.sendWidgetText(s"Search results for '$searchItem' ...", 5383)
     TEXT_WIDGETS.foreach(plr.queue(_))
-    plr.queue(new WidgetItemsMessageWriter(5382, displayBuffer.toArray))
-    this
+    plr.queue(new WidgetItemsMessageWriter(5382, displayArray))
   }
 
-  override def onClose(player: Player) = {
-    plr.queue(new WidgetTextMessageWriter("The Bank of Runescape", 5383))
-    plr.queue(new WidgetTextMessageWriter("Withdraw as:", 5388))
-    plr.queue(new WidgetTextMessageWriter("Item", 5389))
-    plr.queue(new WidgetTextMessageWriter("Rearrange mode:", 5390))
-    plr.queue(new WidgetTextMessageWriter("Note", 5391))
-    plr.queue(new WidgetTextMessageWriter("Insert", 8132))
-    plr.queue(new WidgetTextMessageWriter("Swap", 8133))
+  override def onClose(plr: Player) = {
+    plr.sendWidgetText("The Bank of Runescape", 5383)
+    plr.sendWidgetText("Withdraw as:", 5388)
+    plr.sendWidgetText("Item", 5389)
+    plr.sendWidgetText("Rearrange mode:", 5390)
+    plr.sendWidgetText("Note", 5391)
+    plr.sendWidgetText("Insert", 8132)
+    plr.sendWidgetText("Swap", 8133)
   }
 }
 
@@ -62,8 +63,7 @@ onargs[CommandEvent]("search_item", RIGHTS_DEV) { msg =>
   val plr = msg.plr
   val search = msg.args(0).replaceAll("_", " ")
   if (search.length > 1) {
-    val searchResults = new SearchResultInterface(plr, search)
-    plr.interfaces.open(searchResults.init)
+    plr.interfaces.open(new SearchResultInterface(search))
   } else {
     plr.sendMessage("Search term must be more than 1 character.")
   }
