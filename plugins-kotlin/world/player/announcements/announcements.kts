d182baeebea9ecea91a0b5bbe94240767fd470c3
lare96 <lare96@live.com>
2018-11-28 16:50:30 -0500

    Announcement plugin full migration

--- plugins-kotlin/world/player/announcements/announcements.kts
+++ plugins-kotlin/world/player/announcements/announcements.kts
@@ -1,28 +1,32 @@
 import api.*
 import io.luna.game.event.impl.ServerLaunchEvent
-import io.luna.game.model.mob.Player
 
-
-/* Announcement broadcast interval. */
+/**
+ * Announcement broadcast interval.
+ */
 private val tickInterval = 1500 // 15 mins
 
-/* Messages that will be randomly announced. */
-private val messagesToSend = listOf("Luna is a Runescape private server for the #317 protocol.",
+/**
+ * Messages that will be randomly announced.
+ */
+private val messagesToSend = listOf(
+        "Luna is a Runescape private server designed for the #317 protocol.",
         "Luna can be found on GitHub under luna-rs/luna",
-        "Change these messages in /plugins/world/announcements/announcements.sc",
+        "Change these messages in the 'Announcements' plugin.",
         "Any bugs found using Luna should be reported to the GitHub page.")
 
-/* A filter for players that will receive the announcement. */
-private val playerFilter: (Player) -> Boolean = { it.rights < RIGHTS_ADMIN }
-
+/**
+ * Sends one global message, randomly selected from the list of messages.
+ */
 fun sendMessages() {
-    world.players.filter(playerFilter).forEach {
-        it.sendMessage(messagesToSend.random())
-    }
+    world.players
+        .filter { it.rights < RIGHTS_ADMIN } // Only send if < Admin rank.
+        .forEach { it.sendMessage(messagesToSend.random()) }
 }
 
+/**
+ * Schedules a task that will send global messages.
+ */
 on(ServerLaunchEvent::class).run {
-    world.scheduleForever(tickInterval) {
-        sendMessages()
-    }
+    world.scheduleForever(tickInterval) { sendMessages() }
 }
