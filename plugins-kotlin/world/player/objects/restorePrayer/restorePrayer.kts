87f48053125c33d44e4fd6f54b738b03dfb91fc7
lare96 <lare96@live.com>
2018-11-29 04:22:38 -0500

    Restore prayer plugin full migration

---
+++ plugins-kotlin/world/player/objects/restorePrayer/restorePrayer.kts
@@ -0,0 +1,33 @@
+import api.*
+import io.luna.game.event.impl.ObjectClickEvent.ObjectFirstClickEvent
+import io.luna.game.model.mob.Animation
+import io.luna.game.model.mob.Player
+
+/**
+ * The restore prayer animation.
+ */
+private val prayerAnimation = Animation(645)
+
+/**
+ * Restores the player's prayer points.
+ */
+fun restore(plr: Player) {
+    val prayer = plr.skill(SKILL_PRAYER)
+    if (prayer.level < prayer.staticLevel) {
+        prayer.level = prayer.staticLevel
+        plr.animation(prayerAnimation)
+        plr.sendMessage("You restore your Prayer points.")
+    } else {
+        plr.sendMessage("You already have full Prayer points.")
+    }
+}
+
+/**
+ * Restore prayer points if clicking on correct object.
+ */
+on(ObjectFirstClickEvent::class)
+    .args(409, 3243)
+    .run {
+        restore(it.plr)
+        it.terminate()
+    }
