1faf93052af8cfd512c89380be3934730a7325e5
lare96 <lare96@live.com>
2018-12-04 23:33:59 -0500

    Better documentation for AttributeDsl, better naming

--- plugins/api/AttributeDsl.kt
+++ plugins/api/AttributeDsl.kt
@@ -4,7 +4,18 @@ import io.luna.game.model.mob.Player
 import kotlin.reflect.KProperty
 
 /**
- * A delegate that will retrieve and set attribute values.
+ * A model representing a delegate for attributes to be retrieved and set through properties. The syntax for
+ * usage is as follows
+ *
+ * ```
+ *
+ * var Player.myAttribute by Attr<Int>("attribute_name")
+ *
+ * fun doSomething(plr: Player) {
+ *     plr.myAttribute = 10
+ *     println(plr.myAttribute)
+ * }
+ * ```
  */
 class Attr<T>(private val name: String) {
 
@@ -22,10 +33,25 @@ class Attr<T>(private val name: String) {
 }
 
 /**
- * A delegate for timer attributes.
+ * A model representing a delegate for attribute timers. It includes functions for measuring elapsed time and
+ * being reset. The syntax for usage is as follows
+ *
+ * ```
+ *
+ * var Player.myWatch by Stopwatch("stopwatch_name")
+ *
+ * fun doSomething(plr: Player) {
+ *     // Check if elapsed time > 2500ms
+ *     if(plr.myWatch > 2500) {
+ *         ...
+ *
+ *         // Reset the elapsed time to 0
+ *         plr.myWatch = -1
+ *     }
+ * }
+ * ```
  */
-class TimerAttr(private val name: String) {
-    // TODO Better solution for timer attributes?
+class Stopwatch(private val name: String) {
 
     /**
      * Retrieves the difference between now and the last call to [setValue].
@@ -39,17 +65,11 @@ class TimerAttr(private val name: String) {
     }
 
     /**
-     * Resets the value to [currentTimeMs]. A value other than [RESET_TIMER] will have no effect.
+     * Resets the value to [currentTimeMs], regardless of the [value] argument. The recommended convention
+     * is to simply assign -1.
      */
     operator fun setValue(player: Player, property: KProperty<*>, value: Long) {
-        if (value == RESET_TIMER) {
-            val attr = player.attributes.get<Long>(name)
-            attr.set(currentTimeMs())
-        }
+        val attr = player.attributes.get<Long>(name)
+        attr.set(currentTimeMs())
     }
 }
-
-/**
- * A value passed to reset all delegate [Timer] properties.
- */
-const val RESET_TIMER: Long = -1
