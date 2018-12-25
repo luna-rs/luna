4e742582050372210bd6ce7e22fc76fb8ed45361
lare96 <lare96@live.com>
2018-12-25 17:23:30 -0500

    Updates to reflection utilities

--- src/main/java/io/luna/util/ReflectionUtils.java
+++ src/main/java/io/luna/util/ReflectionUtils.java
@@ -1,13 +1,25 @@
 package io.luna.util;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 /**
  * A static-utility class that contains functions for manipulating the reflection API.
  *
  * @author lare96 <http://github.com/lare96>
  */
-public class ReflectionUtils {
+public final class ReflectionUtils {
+
+    /**
+     * A {@link RuntimeException} implementation that acts as a non-checked version of
+     * {@link ReflectiveOperationException}.
+     */
+    public static class ReflectionException extends RuntimeException {
+        public ReflectionException(ReflectiveOperationException e) {
+            super(e);
+        }
+    }
 
     /**
      * Reflectively retrieves a static field from {@code fromClass}.
@@ -16,15 +28,13 @@ public class ReflectionUtils {
      * @param name The name of the field to retrieve.
      * @param fieldType The type of the field to retrieve.
      */
-    public static <T> T getStaticField(Class<?> fromClass, String name, Class<T> fieldType) {
+    public static <T> T getStaticField(Class<?> fromClass, String name, Class<T> fieldType) throws ReflectionException {
         try {
             Field field = fromClass.getDeclaredField(name);
             field.setAccessible(true);
             return fieldType.cast(field.get(null));
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        } catch (NoSuchFieldException e) {
-            throw new TypeNotPresentException(name, e);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new ReflectionException(e);
         }
     }
 
@@ -35,15 +45,13 @@ public class ReflectionUtils {
      * @param name The name of the field to retrieve.
      * @param fieldType The type of the field to retrieve.
      */
-    public static <T> T getField(Object instance, String name, Class<T> fieldType) {
+    public static <T> T getField(Object instance, String name, Class<T> fieldType) throws ReflectionException {
         try {
             Field field = instance.getClass().getDeclaredField(name);
             field.setAccessible(true);
             return fieldType.cast(field.get(instance));
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        } catch (NoSuchFieldException e) {
-            throw new TypeNotPresentException(name, e);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new ReflectionException(e);
         }
     }
 
@@ -54,15 +62,31 @@ public class ReflectionUtils {
      * @param name The name of the field to set.
      * @param newValue The new value to set to the field.
      */
-    public static void setField(Object instance, String name, Object newValue) {
+    public static void setField(Object instance, String name, Object newValue) throws ReflectionException {
         try {
             Field field = instance.getClass().getDeclaredField(name);
             field.setAccessible(true);
             field.set(instance, newValue);
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        } catch (NoSuchFieldException e) {
-            throw new TypeNotPresentException(name, e);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new ReflectionException(e);
+        }
+    }
+
+    /**
+     * Reflectively creates a new instance of {@code className} using {@code parameters}.
+     *
+     * @param className The fully qualified class name.
+     * @param parameters The parameters, if any.
+     */
+    public static <T> T newInstanceOf(String className, Object... parameters) throws ClassCastException, ReflectionException {
+        try {
+            Class[] parameterTypes = Arrays.stream(parameters).
+                    map(Object::getClass).toArray(Class[]::new);
+            Class forClass = Class.forName(className);
+            Constructor<T> constructor = forClass.getConstructor(parameterTypes);
+            return constructor.newInstance(parameters);
+        } catch (ReflectiveOperationException e) {
+            throw new ReflectionException(e);
         }
     }
 }
