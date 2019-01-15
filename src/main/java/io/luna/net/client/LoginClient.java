ae5820dc5ac524dbea7df4f7caf937fdc1fb4dae
lare96 <lare96@live.com>
2019-01-15 03:01:40 -0500

    Login changes

--- src/main/java/io/luna/net/client/LoginClient.java
+++ src/main/java/io/luna/net/client/LoginClient.java
@@ -1,12 +1,13 @@
 package io.luna.net.client;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import io.luna.LunaContext;
 import io.luna.game.model.World;
 import io.luna.game.model.mob.Player;
 import io.luna.game.model.mob.PlayerCredentials;
 import io.luna.game.model.mob.PlayerRights;
-import io.luna.game.model.mob.PlayerSerializer;
-import io.luna.game.model.mob.attr.AttributeValue;
 import io.luna.net.LunaChannelFilter;
 import io.luna.net.codec.game.GameMessageDecoder;
 import io.luna.net.codec.game.GameMessageEncoder;
@@ -20,10 +21,6 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 
-import java.time.LocalDate;
-import java.util.Optional;
-import java.util.function.Function;
-
 import static com.google.common.base.Preconditions.checkState;
 
 /**
@@ -34,10 +31,9 @@ import static com.google.common.base.Preconditions.checkState;
 public class LoginClient extends Client<LoginCredentialsMessage> {
 
     /**
-     * A listener ran once the login response message is sent to the client. Its primary function is to
-     * add new codecs and prepare the Player for gameplay.
+     * A callback that sends the login response after deserialization.
      */
-    private final class LoginResponseFlushListener implements GenericFutureListener<Future<Void>> {
+    private final class LoginResponseCallback implements FutureCallback<LoginResponse> {
 
         /**
          * The player.
@@ -50,12 +46,56 @@ public class LoginClient extends Client<LoginCredentialsMessage> {
         private final LoginCredentialsMessage msg;
 
         /**
-         * Creates a new {@link LoginResponseFlushListener}.
+         * Creates a new {@link LoginCredentialsMessage}.
          *
          * @param player The player.
          * @param msg The login credentials.
          */
-        private LoginResponseFlushListener(Player player, LoginCredentialsMessage msg) {
+        private LoginResponseCallback(Player player, LoginCredentialsMessage msg) {
+            this.player = player;
+            this.msg = msg;
+        }
+
+        @Override
+        public void onSuccess(LoginResponse result) {
+            sendLoginResponse(player, result, msg);
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            sendLoginResponse(player, LoginResponse.COULD_NOT_COMPLETE_LOGIN, msg);
+        }
+
+        /**
+         * Initializes this callback using the argued future.
+         */
+        public void init(ListenableFuture<LoginResponse> loadFuture) {
+            Futures.addCallback(loadFuture, this, channel.eventLoop());
+        }
+    }
+
+    /**
+     * A listener that loads the player after the login response is sent.
+     */
+    private final class LoadPlayerListener implements GenericFutureListener<Future<Void>> {
+
+        /**
+         * The player.
+         */
+        private final Player player;
+
+        /**
+         * The login credentials.
+         */
+        private final LoginCredentialsMessage msg;
+
+        /**
+         * Creates a new {@link LoadPlayerListener}.
+         *
+         * @param player The player.
+         * @param msg The login credentials.
+         */
+        private LoadPlayerListener(Player player, LoginCredentialsMessage msg) {
             this.player = player;
             this.msg = msg;
         }
@@ -125,62 +165,33 @@ public class LoginClient extends Client<LoginCredentialsMessage> {
         }
 
         // Determine login response and send it to the client.
-        LoginResponse response = getLoginResponse(player);
-        LoginResponseMessage responseMsg = new LoginResponseMessage(response, player.getRights(), false);
-
-        channel.writeAndFlush(responseMsg).addListener(response == LoginResponse.NORMAL ?
-                new LoginResponseFlushListener(player, msg) : ChannelFutureListener.CLOSE);
+        if (world.getPlayers().isFull()) {
+            // World is full.
+            sendLoginResponse(player, LoginResponse.WORLD_FULL, msg);
+        } else if (world.getPlayer(player.getUsernameHash()).isPresent() ||
+                world.getPersistence().hasPendingSave(player)) {
+            // Player is online, or their file is still being saved.
+            sendLoginResponse(player, LoginResponse.ACCOUNT_ONLINE, msg);
+        } else if (player.isBanned()) {
+            // Player is banned.
+            sendLoginResponse(player, LoginResponse.ACCOUNT_BANNED, msg);
+        } else {
+            // Load persistent player data.
+            LoginResponseCallback loadCallback = new LoginResponseCallback(player, msg);
+            loadCallback.init(world.loadPlayer(player));
+        }
     }
 
     /**
      * Determines the login response for {@code player}.
      *
      * @param player The player.
-     * @return The login response.
+     * @param response The login response.
+     * @param msg The login credentials message.
      */
-    private LoginResponse getLoginResponse(Player player) {
-        if (world.getPlayers().isFull()) {
-            return LoginResponse.WORLD_FULL;
-        } else if (world.getPlayer(player.getUsernameHash()).isPresent()) {
-            return LoginResponse.ACCOUNT_ONLINE;
-        } else {
-            PlayerSerializer deserializer = new PlayerSerializer(player);
-            LoginResponse response = deserializer.load(player.getPassword());
-            return handlePunishments(player).orElse(response);
-        }
-    }
-
-
-    /**
-     * Returns an optional describing the result of managing punishments.
-     *
-     * @param player The player.
-     * @return The login response, wrapped in an optional.
-     */
-    private Optional<LoginResponse> handlePunishments(Player player) {
-        // TODO rewrite, should also be done in a plugin...
-        LocalDate now = LocalDate.now();
-        Function<String, LocalDate> liftFunc =
-                it -> it.equals("never") ? now.plusYears(1) : LocalDate.parse(it);
-
-        if (player.isBanned()) {
-            LocalDate liftDate = liftFunc.apply(player.getUnbanDate());
-            if (now.isBefore(liftDate)) {
-                return Optional.of(LoginResponse.ACCOUNT_BANNED);
-            } else {
-                // Player has been unbanned.
-                player.setUnbanDate("n/a");
-            }
-        }
-
-        if (player.isMuted()) {
-            LocalDate liftDate = liftFunc.apply(player.getUnmuteDate());
-            if (liftDate.isBefore(now)) {
-                // Player has been unmuted.
-                AttributeValue<String> attr = player.getAttributes().get("unmute_date");
-                attr.set("n/a");
-            }
-        }
-        return Optional.empty();
+    private void sendLoginResponse(Player player, LoginResponse response, LoginCredentialsMessage msg) {
+        LoginResponseMessage responseMsg = new LoginResponseMessage(response, player.getRights(), false);
+        channel.writeAndFlush(responseMsg).addListener(response == LoginResponse.NORMAL ?
+                new LoadPlayerListener(player, msg) : ChannelFutureListener.CLOSE);
     }
 }
