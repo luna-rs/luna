d18721b7c351a717ce85e7ead9e5b88da2edb77f
lare96 <lare96@live.com>
2019-01-16 00:24:14 -0500

    Fix packet 126 caching bug

--- plugins/world/player/command/punishment/punishmentCmd.kts
+++ plugins/world/player/command/punishment/punishmentCmd.kts
@@ -4,17 +4,23 @@ import io.luna.game.event.impl.CommandEvent
 import io.luna.game.model.mob.Player
 import java.io.File
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+/**
+ * The date formatter.
+ */
+val df = DateTimeFormatter.ofPattern("MMMM d, uuuu")!!
 
 /**
  * Performs a lookup for the person we're punishing.
  */
 fun getPlayer(msg: CommandEvent, action: (Player) -> Unit) =
-    world.getPlayer(msg.name).ifPresent(action)
+    world.getPlayer(msg.args[0]).ifPresent(action)
 
 /**
  * Construct a string with punishment lift date ~ [yyyy-mm-dd].
  */
-fun punishDuration(msg: CommandEvent): String {
+fun punishDuration(msg: CommandEvent): LocalDate {
     val args = msg.args
     val years = if (args.size == 4) args[3].toLong() else 0
     val months = if (args.size == 3) args[2].toLong() else 0
@@ -24,7 +30,6 @@ fun punishDuration(msg: CommandEvent): String {
         .plusYears(years)
         .plusMonths(months)
         .plusDays(days)
-        .toString()
 }
 
 /**
@@ -37,6 +42,7 @@ cmd("ip_ban", RIGHTS_ADMIN) {
             Files.write(writeString.toByteArray(), File("./data/players/blacklist.txt"))
         }
         it.logout()
+        plr.sendMessage("You have IP banned ${it.username}.")
     }
 }
 
@@ -47,6 +53,7 @@ cmd("perm_ban", RIGHTS_ADMIN) {
     getPlayer(this) {
         it.unbanDate = "never"
         it.logout()
+        plr.sendMessage("You have permanently banned ${it.username}.")
     }
 }
 
@@ -57,6 +64,7 @@ cmd("perm_mute", RIGHTS_MOD) {
     getPlayer(this) {
         it.unmuteDate = "never"
         it.logout()
+        plr.sendMessage("You have permanently muted ${it.username}.")
     }
 }
 
@@ -65,8 +73,10 @@ cmd("perm_mute", RIGHTS_MOD) {
  */
 cmd("ban", RIGHTS_MOD) {
     getPlayer(this) {
-        it.unbanDate = punishDuration(this)
+        val duration = punishDuration(this)
+        it.unbanDate = duration.toString()
         it.logout()
+        plr.sendMessage("You have banned ${it.username} until ${df.format(duration)}.")
     }
 }
 
@@ -75,8 +85,10 @@ cmd("ban", RIGHTS_MOD) {
  */
 cmd("mute", RIGHTS_MOD) {
     getPlayer(this) {
-        it.unmuteDate = punishDuration(this)
+        val duration = punishDuration(this)
+        it.unmuteDate = duration.toString()
         it.logout()
+        plr.sendMessage("You have muted ${it.username} until ${df.format(duration)}.")
     }
 }
 
@@ -84,5 +96,8 @@ cmd("mute", RIGHTS_MOD) {
  * Perform a forced disconnect on a player.
  */
 cmd("kick", RIGHTS_MOD) {
-    getPlayer(this) { it.logout() }
+    getPlayer(this) {
+        it.logout()
+        plr.sendMessage("You have kicked ${it.username}.")
+    }
 }
