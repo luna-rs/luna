074f74c5d7a31ff0df924c139dfbfa526252f7bb
lare96 <lare96@live.com>
2019-01-20 01:51:58 -0500

    Decouple ::search_item command from banking

--- plugins/world/player/command/developer/searchItem/searchItemCmd.kts
+++ plugins/world/player/command/developer/searchItem/searchItemCmd.kts
@@ -1,61 +1,28 @@
 import api.predef.*
-import io.luna.game.model.def.ItemDefinition
+import io.luna.game.event.impl.WidgetItemClickEvent
+import io.luna.game.event.impl.WidgetItemClickEvent.*
 import io.luna.game.model.item.Item
 import io.luna.game.model.mob.Player
-import io.luna.game.model.mob.inter.StandardInterface
-import io.luna.net.msg.out.WidgetItemsMessageWriter
-import io.luna.net.msg.out.WidgetTextMessageWriter
-import kotlin.streams.toList
+import io.luna.game.model.mob.inter.AmountInputInterface
+import world.player.command.developer.searchItem.SearchResultInterface
 
 /**
- * A standard interface representing the result of a search.
+ * Spawn an item.
  */
-class SearchResultInterface(val searchToken: String) : StandardInterface(5292) {
-
-    /**
-     * A list of empty text widget messages.
-     */
-    val clearWidgets = mutableListOf(5388, 5389, 5390, 5391, 8132, 8133)
-        .map { WidgetTextMessageWriter("", it) }
-
-    override fun onOpen(plr: Player) {
-
-        // Perform search here, filter items based on search token.
-        val itemsFound = ItemDefinition.ALL
-            .stream()
-            .filter { it.id > 0 && !it.isNoted && it.name.toLowerCase().contains(searchToken) }
-            .map { Item(it.id, Int.MAX_VALUE) }
-            .toList()
-
-        // Display the items, truncating results if necessary.
-        fun display(items: List<Item>) =
-            plr.queue(WidgetItemsMessageWriter(5382, items))
-
-        val resultCount = itemsFound.size
-        val maxResults = plr.bank.capacity()
-        if (resultCount > maxResults) {
-            // Truncate results.
-            plr.sendMessage("Too many results ($resultCount) to be displayed! The search has been truncated.")
-            display(itemsFound.dropLast(resultCount - maxResults))
-        } else {
-            // Display as usual.
-            plr.sendMessage("Found $resultCount results for search term '$searchToken'.")
-            display(itemsFound)
-        }
-        clearWidgets.forEach { plr.queue(it) }
-        plr.sendText("Search results for '$searchToken' ...", 5383)
-    }
-
-    override fun onClose(plr: Player) {
-        plr.sendText("The Bank of Runescape", 5383)
-        plr.sendText("Withdraw as:", 5388)
-        plr.sendText("Item", 5389)
-        plr.sendText("Rearrange mode:", 5390)
-        plr.sendText("Note", 5391)
-        plr.sendText("Insert", 8132)
-        plr.sendText("Swap", 8133)
+fun spawn(msg: WidgetItemClickEvent, amount: Int? = null) {
+    val plr = msg.plr
+    val id = msg.itemId
+    val inv = plr.inventory
+    when (amount) {
+        null -> inv.add(Item(id, Int.MAX_VALUE))
+        else -> inv.add(Item(id, amount))
     }
 }
+
+/**
+ * Determines if the search interface is open.
+ */
+fun isInterfaceOpen(plr: Player) = plr.interfaces.isOpen(SearchResultInterface::class)
 
 /**
  * A command that displays items on the banking interface, for easier item spawning.
@@ -68,3 +35,42 @@ cmd("search_item", RIGHTS_DEV) {
         plr.sendMessage("Search term must be more than 1 character.")
     }
 }
+
+/**
+ * Spawn 1.
+ */
+on(WidgetItemFirstClickEvent::class)
+    .filter { widgetId == 5382 && isInterfaceOpen(plr) }
+    .then { spawn(this, 1) }
+
+/**
+ * Spawn 5.
+ */
+on(WidgetItemSecondClickEvent::class)
+    .filter { widgetId == 5382 && isInterfaceOpen(plr) }
+    .then { spawn(this, 5) }
+
+/**
+ * Spawn 10.
+ */
+on(WidgetItemThirdClickEvent::class)
+    .filter { widgetId == 5382 && isInterfaceOpen(plr) }
+    .then { spawn(this, 10) }
+
+/**
+ * Spawn all.
+ */
+on(WidgetItemFourthClickEvent::class)
+    .filter { widgetId == 5382 && isInterfaceOpen(plr) }
+    .then { spawn(this) }
+
+/**
+ * Spawn (x).
+ */
+on(WidgetItemFifthClickEvent::class)
+    .filter { widgetId == 5382 && isInterfaceOpen(plr) }
+    .then {
+        plr.interfaces.open(object : AmountInputInterface() {
+            override fun onAmountInput(player: Player, value: Int) = spawn(this@then, value)
+        })
+    }
