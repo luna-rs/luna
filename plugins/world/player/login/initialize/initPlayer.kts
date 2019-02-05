d0f0d8b2223d8ba75b3b914e31d560482fbc2eed
lare96 <lare96@live.com>
2019-02-05 12:56:53 -0500

    Decoupled login plugin

---
+++ plugins/world/player/login/initialize/initPlayer.kts
@@ -0,0 +1,57 @@
+import api.predef.*
+import io.luna.game.event.impl.LoginEvent
+import io.luna.game.model.mob.Player
+import io.luna.net.msg.out.AssignmentMessageWriter
+import io.luna.net.msg.out.SkillUpdateMessageWriter
+import io.luna.net.msg.out.UpdateRunEnergyMessageWriter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+/**
+ * Formats dates into the specified pattern.
+ */
+val dateFormatter = DateTimeFormatter.ofPattern("MMMM d, uuuu")!!
+
+/**
+ * If the player is muted, send the mute details.
+ */
+fun checkMute(plr: Player) {
+    if (plr.isMuted) {
+        when (plr.unmuteDate) {
+            "never" -> plr.sendMessage("You are permanently muted. You cannot appeal this.")
+            else -> {
+                val lift = LocalDate.parse(plr.unmuteDate)
+                plr.sendMessage("You are muted. You will be unmuted on ${dateFormatter.format(lift)}.")
+            }
+        }
+    }
+}
+
+/**
+ * Final initialization of the player before gameplay.
+ */
+fun init(plr: Player) {
+    plr.tabs.resetAll()
+
+    plr.interactions.show(INTERACTION_FOLLOW)
+    plr.interactions.show(INTERACTION_TRADE)
+
+    plr.equipment.loadBonuses()
+    plr.inventory.refreshPrimary(plr)
+    plr.equipment.refreshPrimary(plr)
+
+    plr.queue(UpdateRunEnergyMessageWriter())
+    plr.queue(AssignmentMessageWriter(true))
+
+    plr.skills.forEach { plr.queue(SkillUpdateMessageWriter(it.id)) }
+
+    plr.sendMessage("Welcome to Luna.")
+    plr.sendMessage("You currently have " + plr.rights.formattedName + " privileges.")
+
+    checkMute(plr)
+}
+
+/**
+ * Listens for login events, initializes [Player]s.
+ */
+on(LoginEvent::class) { init(plr) }
---
+++ plugins/world/player/login/starterPackage/starterPackage.kts
@@ -0,0 +1,55 @@
+import api.attr.Attr
+import api.predef.*
+import io.luna.game.event.impl.LoginEvent
+import io.luna.game.model.item.Item
+import io.luna.game.model.mob.Player
+import io.luna.game.model.mob.PlayerAppearance.DesignPlayerInterface
+
+/**
+ * The "first_login" attribute.
+ */
+var Player.firstLogin by Attr<Boolean>("first_login")
+
+/**
+ * Inventory starter items.
+ */
+val inventoryStarter = listOf(
+        Item(995, 10000), // Coins
+        Item(556, 250), // Air runes
+        Item(555, 250), // Water runes
+        Item(554, 250), // Fire runes
+        Item(557, 250), // Earth runes
+        Item(558, 500), // Mind runes
+        Item(841)) // Shortbow
+
+/**
+ * Equipment starter items.
+ */
+val equipmentStarter = listOf(
+        Item(1153), // Iron full helm
+        Item(1115), // Iron platebody
+        Item(1067), // Iron platelegs
+        Item(1323), // Iron scimitar
+        Item(1191), // Iron kiteshield
+        Item(1731), // Amulet of power
+        Item(4121), // Iron boots
+        Item(1063), // Leather vambraces
+        Item(2570), // Ring of life
+        Item(1019), // Black cape
+        Item(882, 750)) // Bronze arrows
+
+/**
+ * Called when the player logs in for the first time.
+ */
+fun firstLogin(plr: Player) {
+    plr.sendMessage("This is your first login. Enjoy your starter package!")
+
+    plr.inventory.addAll(inventoryStarter)
+    plr.equipment.addAll(equipmentStarter)
+    plr.interfaces.open(DesignPlayerInterface())
+    plr.firstLogin = false
+}
+
+on(LoginEvent::class)
+    .condition { plr.firstLogin }
+    .then { firstLogin(plr) }
