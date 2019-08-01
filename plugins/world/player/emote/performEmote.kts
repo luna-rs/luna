97ae83573e8d5b809ae831e75d94f284a722d80d
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-08-01 19:20:43 -0400

    add player emote plugin

---
+++ plugins/world/player/emote/performEmote.kts
@@ -0,0 +1,20 @@
+import api.predef.*
+import io.luna.game.event.impl.ButtonClickEvent
+import io.luna.game.model.mob.Animation
+import io.luna.game.model.mob.Player
+import world.player.emote.Emote
+
+/**
+ * Performs an emote if the pressed button corresponds to an emote button.
+ */
+fun performEmote(plr: Player, button: Int) {
+    val emote = Emote.BUTTON_TO_EMOTE[button];
+    if (emote != null) {
+        plr.animation(Animation(emote.id));
+    }
+}
+
+/**
+ * Forward to [performEmote].
+ */
+on(ButtonClickEvent::class) { performEmote(plr, id) }
