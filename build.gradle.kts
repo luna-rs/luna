a87dbcb739ffd66dc871012c6658f0587c153891
Juan Ortiz <jportiz32@gmail.com>
2019-10-19 14:08:34 -0400

    Migrate to gradle (#163)
    * #146 Add gradle components (using gradle init)
    
    * #146 Convert groovy scripts to kotlin
    
    This also includes changes to the configurations to further resemble the existing maven ones.
    
    * Update .gitignore
    
    Contributes to #146
    Ignore Gradle, Java, Eclipse, and Intellij auto-generated files
    
    * Update build.gradle.kts
    
    Contributes to #146
    Includes:
    * Reconfiguration of test tasks
    * Define Kotlin JVM target
    * Addition of new test dependency (junit-jupiter-engine)
    
    * Remove pom.xml
    
    * Assign kotlin source directory to 'plugins' folder
    
    * Add UTF-8 encoding for all tasks requiring java to compile
    
    * Code clean-up and reformatting
    
    * Update .travis.yml
    
    - Adds directories for gradle caches and wrapper
    - Grants execution permission for gradle scripts
    - Builds application
    
    * Define operating system for CI build
    
    * Grant execution permissions to the gradlew file itself.
    
    * Revert "Revert "Merge branch 'master' into feature/migrate-to-gradle""
    
    This reverts commit 533e14ad
    
    * Revert "Merge branch 'master' into feature/migrate-to-gradle"
    
    This reverts commit 85323467

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.3.11"
+    val jfxVersion = "0.0.8"
+
+    java
+    application
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.openjfx.javafxplugin") version jfxVersion
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("http://repo.maven.apache.org/maven2")
+    }
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("org.apache.logging.log4j:log4j-core:2.11.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.11.1")
+    implementation("org.slf4j:slf4j-nop:1.7.25")
+    implementation("com.lmax:disruptor:3.4.2")
+    implementation("io.netty:netty-all:4.1.32.Final")
+    implementation("com.google.guava:guava:27.0.1-jre")
+    implementation("com.moandjiezana.toml:toml4j:0.7.2")
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.11")
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.3.11")
+    implementation("org.jetbrains.kotlin:kotlin-script-util:1.3.11")
+    implementation("org.jetbrains.kotlin:kotlin-compiler:1.3.11")
+    implementation("org.openjfx:javafx-controls:11.0.1")
+    implementation("org.openjfx:javafx-fxml:11.0.1")
+    implementation("org.openjfx:javafx-swing:11.0.1")
+    implementation("com.zaxxer:HikariCP:3.3.0")
+    implementation("org.mockito:mockito-core:2.24.5")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:+")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:+")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:+")
+}
+
+group = "luna"
+version = "1.0"
+
+application {
+    mainClassName = "io.luna.Luna"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+sourceSets {
+    main {
+        withConvention(KotlinSourceSet::class) {
+            kotlin.srcDirs("plugins")
+        }
+    }
+}
+
+javafx {
+    version = "11"
+    modules("javafx.controls", "javafx.fxml", "javafx.swing")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs = MutableList(1) { "-Xlint:unchecked" }
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
