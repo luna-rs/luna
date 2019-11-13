b2b80d07ee4732b4d644030c91af51f7b12a7992
Juan Ortiz <jportiz32@gmail.com>
2019-11-12 19:47:16 -0500

    Fix logic for querying player by id.

--- src/main/java/io/luna/net/msg/in/CastOnPlayerMessageReader.java
+++ src/main/java/io/luna/net/msg/in/CastOnPlayerMessageReader.java
@@ -2,29 +2,31 @@ package io.luna.net.msg.in;
 
 import io.luna.game.event.Event;
 import io.luna.game.event.impl.CastOnPlayerEvent;
-import io.luna.game.model.World;
+import io.luna.game.model.mob.MobList;
 import io.luna.game.model.mob.Player;
 import io.luna.net.codec.ByteOrder;
 import io.luna.net.codec.ValueType;
 import io.luna.net.msg.GameMessage;
 import io.luna.net.msg.GameMessageReader;
 
+import java.util.Optional;
+
 public final class CastOnPlayerMessageReader extends GameMessageReader {
 
     @Override
-    public Event read(Player player, GameMessage msg) {
+    public Event read(Player caster, GameMessage msg) {
         int targetPlayerId = msg.getPayload().getShort(false, ValueType.ADD); // the id of the player
         // targeted
         int targetSpellId = msg.getPayload().getShort(false, ByteOrder.LITTLE); // the id of the spell casted
 
-        if (!targetExistsInWorld(targetPlayerId, player.getWorld())) {
+        MobList<Player> players = caster.getWorld().getPlayers();
+        Optional<Player> targetPlayer = players.retrieve(targetPlayerId);
+
+        if (targetPlayer.isEmpty()) {
             throw new InvalidSpellTargetException(targetPlayerId);
         }
-        return new CastOnPlayerEvent(player, targetSpellId, targetPlayerId);
-    }
 
-    private boolean targetExistsInWorld(int targetPlayerId, World world) {
-        return world.getPlayers().stream().anyMatch(player -> player.getDatabaseId() == targetPlayerId);
+        return new CastOnPlayerEvent(caster, targetSpellId, targetPlayerId);
     }
 
     private static class InvalidSpellTargetException extends RuntimeException {
@@ -32,5 +34,4 @@ public final class CastOnPlayerMessageReader extends GameMessageReader {
             super("Target no longer exists.");
         }
     }
-
 }
