0265b95dbeda01286d836747144ad8dd72c324b2
Juan Ortiz <jportiz32@gmail.com>
2019-11-17 15:25:40 -0500

    Add test suite for `Position`

--- src/test/java/io/luna/game/model/PositionTest.java
+++ src/test/java/io/luna/game/model/PositionTest.java
@@ -2,7 +2,8 @@ package io.luna.game.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 /**
  * Unit tests for {@link Position}.
@@ -23,11 +24,67 @@ final class PositionTest {
 
     @Test
     void invalidUpperZ() {
-        assertThrows(IllegalArgumentException.class, () -> new Position(1, 1, 4));
+        assertThrows(IllegalArgumentException.class, () -> new Position(1, 1, 4),
+                "A position's Z axis (height) cannot exceed 3.");
     }
 
     @Test
     void invalidLowerZ() {
         assertThrows(IllegalArgumentException.class, () -> new Position(1, 1, -1));
+    }
+
+    @Test
+    void testIsWithinDistance() {
+        Position startPosition = new Position(0, 0, 0);
+
+        // when X is in range, return true
+        Position endPosition = new Position(1, 0, 0);
+        assertTrue(startPosition.isWithinDistance(endPosition, 1));
+
+        // when X is out of range, return false
+        endPosition = endPosition.translate(1, 0);
+        assertFalse(startPosition.isWithinDistance(endPosition, 1));
+
+        // when Y is in range, return true
+        endPosition = new Position(0, 1, 0);
+        assertTrue(startPosition.isWithinDistance(endPosition, 1));
+
+        // when Y is out of range, return false
+        endPosition = endPosition.translate(0, 1);
+        assertFalse(startPosition.isWithinDistance(endPosition, 1));
+
+        // when start and end positions are on different planes, return false.
+        endPosition = new Position(0, 0, 1);
+        assertFalse(startPosition.isWithinDistance(endPosition, 1));
+    }
+
+    @Test
+    void testIsViewable() {
+        Position startPosition = new Position(0, 0, 0);
+
+        // when position is at max view distance, return true
+        Position endPosition = new Position(EntityConstants.VIEWING_DISTANCE, EntityConstants.VIEWING_DISTANCE, 0);
+        assertTrue(startPosition.isViewable(endPosition));
+
+        // when position is 1-unit out of view distance, return false
+        endPosition = endPosition.translate(1, 1);
+        assertFalse(startPosition.isViewable(endPosition));
+    }
+
+    @Test
+    void testGetFarthestCoordinate() {
+        Position startPosition = new Position(0, 0, 0);
+
+        int farthestCoordinate = 1;
+        int closestCoordinate = 0;
+
+        Position endPosition = new Position(closestCoordinate, farthestCoordinate);
+        int computed = startPosition.computeLongestDistance(endPosition);
+        assertEquals(computed, farthestCoordinate);
+
+        // swap X and Y coordinates
+        endPosition = new Position(farthestCoordinate, closestCoordinate);
+        computed = startPosition.computeLongestDistance(endPosition);
+        assertEquals(computed, farthestCoordinate);
     }
 }
