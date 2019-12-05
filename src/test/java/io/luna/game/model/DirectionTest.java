af6e1f568affec8099397defeebe6ab7a9a647d0
Juan <jportiz32@gmail.com>
2019-12-05 15:28:34 -0500

    Apply parameterization for DirectionTest

--- src/test/java/io/luna/game/model/DirectionTest.java
+++ src/test/java/io/luna/game/model/DirectionTest.java
@@ -1,63 +1,39 @@
 package io.luna.game.model;
 
-import io.luna.game.model.mob.WalkingQueue;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DirectionTest {
 
-    private WalkingQueue.Step step1;
-    private WalkingQueue.Step step2;
-
-    private Direction actual; // the calculated direction
-
-    @Test
-    void whenStepIncreasesY_returnNorth() {
-        step1 = new WalkingQueue.Step(0, 0);
-        step2 = new WalkingQueue.Step(0, 1);
-
-        // we're moving from step1 to step, where (0,0) is the origin
-        actual = Direction.between(step1, step2);
-        assertEquals(Direction.NORTH, actual);
-
-        actual = Direction.between(step1.getX(), step1.getY(), step2.getX(), step2.getY());
-        assertEquals(Direction.NORTH, actual);
+    @ParameterizedTest
+    @MethodSource("createStepsWithResult")
+    void testDirectionBetweenSteps(int step1X, int step1Y, int step2X, int step2Y, Direction expected) {
+        Direction actual = Direction.between(step1X, step1Y, step2X, step2Y);
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void whenStepDecreaseY_returnSouth() {
-        step1 = new WalkingQueue.Step(0, 1);
-        step2 = new WalkingQueue.Step(0, 0);
-
-        actual = Direction.between(step1, step2);
-        assertEquals(Direction.SOUTH, actual);
-
-        actual = Direction.between(step1.getX(), step1.getY(), step2.getX(), step2.getY());
-        assertEquals(Direction.SOUTH, actual);
-    }
-
-    @Test
-    void whenStepIncreasesX_returnEast() {
-        step1 = new WalkingQueue.Step(0, 0);
-        step2 = new WalkingQueue.Step(1, 0);
-
-        actual = Direction.between(step1, step2);
-        assertEquals(Direction.EAST, actual);
-
-        actual = Direction.between(step1.getX(), step1.getY(), step2.getX(), step2.getY());
-        assertEquals(Direction.EAST, actual);
-    }
-
-    @Test
-    void whenStepDecreasesX_returnWest() {
-        step1 = new WalkingQueue.Step(1, 0);
-        step2 = new WalkingQueue.Step(0, 0);
-
-        actual = Direction.between(step1, step2);
-        assertEquals(Direction.WEST, actual);
-
-        actual = Direction.between(step1.getX(), step1.getY(), step2.getX(), step2.getY());
-        assertEquals(Direction.WEST, actual);
+    private static Stream<Arguments> createStepsWithResult() {
+        return Stream.of(
+                Arguments.of(0, 0, 0, 0, Direction.NONE),
+                Arguments.of(0, 0, 0, 1, Direction.NORTH),
+                Arguments.of(0, 0, 1, 0, Direction.EAST),
+                Arguments.of(0, 0, 1, 1, Direction.NORTH_EAST),
+                Arguments.of(0, 1, 0, 0, Direction.SOUTH),
+                Arguments.of(0, 1, 0, 1, Direction.NONE),
+                Arguments.of(0, 1, 1, 0, Direction.SOUTH_EAST),
+                Arguments.of(0, 1, 1, 1, Direction.EAST),
+                Arguments.of(1, 0, 0, 0, Direction.WEST),
+                Arguments.of(1, 0, 0, 1, Direction.NORTH_WEST),
+                Arguments.of(1, 0, 1, 0, Direction.NONE),
+                Arguments.of(1, 0, 1, 1, Direction.NORTH),
+                Arguments.of(1, 1, 0, 0, Direction.SOUTH_WEST),
+                Arguments.of(1, 1, 0, 1, Direction.WEST),
+                Arguments.of(1, 1, 1, 0, Direction.SOUTH),
+                Arguments.of(1, 1, 1, 1, Direction.NONE));
     }
 }
