0f219e9405b273db5790c780014ab87b7d3c3f53
lare96 <lare96@live.com>
2019-12-29 15:01:19 -0500

    Herblore build plugin

---
+++ plugins/world/player/skill/herblore/build.plugin.kts
@@ -0,0 +1,10 @@
+package world.player.skill.herblore
+
+import api.bootstrap.plugin
+
+plugin {
+    name = "Herblore"
+    description = "A plugin that enables the Herblore skill."
+    version = "1.0"
+    authors += "lare96"
+}
