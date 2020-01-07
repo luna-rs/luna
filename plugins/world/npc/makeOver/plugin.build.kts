f3e81a236e6d21379c319eac1f3d0392f6537b2f
Dan Searle <dan_searle@outlook.com>
2020-01-07 11:52:02 +0000

    Make-over mage plugin (#197)
    * Make-over mage plugin
    
    Added a "Make-over mage" between the "Shopkeeper" and "Tanner" NPC's.
    
    Issue: #105
    
    * Update to most recent metadata file

---
+++ plugins/world/npc/makeOver/plugin.build.kts
@@ -0,0 +1,13 @@
+package world.npc.makeOver
+
+import api.bootstrap.plugin
+
+plugin {
+    name = "Make-over mage"
+    description =
+        """
+        A plugin that enables a make-over mage at the player spawn location.
+        """
+    version = "1.0"
+    authors += "searledan"
+}
