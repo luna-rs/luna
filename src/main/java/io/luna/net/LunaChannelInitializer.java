dcf1ec774fd5472590496b85c28dc91486a96b68
Juan <jportiz32@gmail.com>
2020-02-29 13:27:00 -0500

    Add unit test for TomlRsaPrivateKeyPairReader

--- src/main/java/io/luna/net/LunaChannelInitializer.java
+++ src/main/java/io/luna/net/LunaChannelInitializer.java
@@ -5,6 +5,9 @@ import io.luna.net.client.Client;
 import io.luna.net.client.IdleClient;
 import io.luna.net.codec.login.LoginDecoder;
 import io.luna.net.codec.login.LoginEncoder;
+import io.luna.net.security.RsaKeyPair;
+import io.luna.net.security.RsaKeyPairReader;
+import io.luna.net.security.TomlRsaPrivateKeyPairReader;
 import io.luna.net.msg.GameMessageRepository;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandler.Sharable;
@@ -47,9 +50,14 @@ public final class LunaChannelInitializer extends ChannelInitializer<SocketChann
     private final GameMessageRepository msgRepository;
 
     /**
+     * The private RSA key pair, only to be seen by the server.
+     */
+    private final RsaKeyPair privateKeyPair;
+
+    /**
      * Creates a new {@link LunaChannelInitializer}.
      *
-     * @param context The context instance.
+     * @param context       The context instance.
      * @param channelFilter A channel handler that will filter channels.
      * @param msgRepository The message repository.
      */
@@ -58,6 +66,9 @@ public final class LunaChannelInitializer extends ChannelInitializer<SocketChann
         this.context = context;
         this.channelFilter = channelFilter;
         this.msgRepository = msgRepository;
+
+        RsaKeyPairReader reader = new TomlRsaPrivateKeyPairReader();
+        this.privateKeyPair = reader.read();
     }
 
     @Override
@@ -67,7 +78,7 @@ public final class LunaChannelInitializer extends ChannelInitializer<SocketChann
 
         ch.pipeline().addLast("read-timeout", new ReadTimeoutHandler(5));
         ch.pipeline().addLast("channel-filter", channelFilter);
-        ch.pipeline().addLast("login-decoder", new LoginDecoder(context, msgRepository));
+        ch.pipeline().addLast("login-decoder", new LoginDecoder(context, msgRepository, privateKeyPair));
         ch.pipeline().addLast("login-encoder", loginEncoder);
         ch.pipeline().addLast("upstream-handler", upstreamHandler);
     }
