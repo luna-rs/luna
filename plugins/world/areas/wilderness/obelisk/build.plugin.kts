6769bdf8ed7a48b39ce54278fe9d051064a00f8b
lare96 <lare96@live.com>
2020-12-31 04:10:27 -0500

    Wilderness obelisks

---
+++ plugins/world/areas/wilderness/obelisk/build.plugin.kts
@@ -0,0 +1,13 @@
+package world.areas.wilderness.obelisk
+
+import api.bootstrap.plugin
+
+plugin {
+    name = "Obelisks"
+    description =
+        """
+        A plugin that enables the functionality of wilderness obelisks.
+        """
+    version = "1.0"
+    authors += "lare96"
+}
