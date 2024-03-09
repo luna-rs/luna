4d0111ce4f1b72cf5cdecc2ddc6ee22a15337672
lare96 <lare96@live.com>
2024-03-09 02:26:46 -0500

    TODO npc drops planning

--- plugins/world/npc/death/dropItems/dropNpcItems.kts
+++ plugins/world/npc/death/dropItems/dropNpcItems.kts
@@ -4,9 +4,14 @@ import api.predef.*
 import io.luna.game.event.impl.NpcDeathEvent
 
 on(NpcDeathEvent::class) {
-    // TODO Npc Drops.
+    val name = npc.definition.name
+    if(source.type == TYPE_PLAYER) {
+        source.asPlr().sendMessage("you killed sum ($name)")
+    } else if(source.isAlive) {
+        source.asNpc().forceChat("i killed sum ($name)")
+    }
 }
-
+// TODO after combat
 // TODO Generialized drop tables should be done in Java for all monsters, a best estimation or even custom made tiers
 // of loot. Then an option to "override" the drop table with specific conditions related to how the mob died, etc. eg.
 // if(player.completedxyz) {
