226179ccd156f0ca7f4012b4d6783a1f9f7e939c
lare96 <lare96@live.com>
2024-12-27 03:27:12 -0500

    Placeholder for Slayer until combat is completed

---
+++ src/main/kotlin/world/player/skill/slayer/info.plugin.kts
@@ -0,0 +1,13 @@
+package world.player.skill.slayer
+
+import api.plugin.plugin
+
+plugin {
+    name = "Slayer"
+    description =
+        """
+        A plugin that enables the Slayer skill. Combat is needed first.
+        """
+    version = "1.0"
+    authors += "lare96" // TODO complete once combat is done
+}
