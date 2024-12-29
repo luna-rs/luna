de9a250f520f330207eb118f3528de66d082c397
lare96 <lare96@live.com>
2024-12-29 00:00:23 -0500

    Remove offer bone custom content until server completion

--- src/main/kotlin/world/player/skill/prayer/info.plugin.kts
+++ src/main/kotlin/world/player/skill/prayer/info.plugin.kts
@@ -8,9 +8,6 @@ plugin {
         """
         A plugin that enables the Prayer skill. This includes
         -> Burying bones
-        -> Using bones on an altar for boosted XP
-            -> Being able to light the altar for boosted XP
-            -> Altar spawn at ;;home
         """
     version = "1.0"
     authors += "lare96"
