eec5911e7f61e77f057869b1b95ab0718199f50b
lare96 <lare96@live.com>
2024-12-28 21:47:46 -0500

    Gradle build file update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,42 @@
-
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.9.23"
-    val jfxVersion = "0.0.8"
+    val kotlinVersion = "1.4.21"
 
     java
     application
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
-    id("org.openjfx.javafxplugin") version jfxVersion
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 val junitVersion: String by project
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.9.1")
     implementation("org.apache.logging.log4j:log4j-core:2.23.1")
     implementation("org.apache.logging.log4j:log4j-api:2.23.1")
     implementation("org.slf4j:slf4j-nop:2.0.12")
     implementation("com.lmax:disruptor:3.4.2")
     implementation("io.netty:netty-all:4.1.107.Final")
-    implementation("com.google.guava:guava:30.1-jre")
-    implementation("com.moandjiezana.toml:toml4j:0.7.2")
-    implementation("org.mindrot:jbcrypt:0.4-atlassian-1")
+    implementation("com.google.guava:guava:33.0.0-jre")
+    implementation("org.mindrot:jbcrypt:0.4")
     implementation("io.github.classgraph:classgraph:4.8.168")
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("script-runtime"))
     implementation(kotlin("reflect"))
     implementation(kotlin("scripting-common"))
-    implementation("org.openjfx:javafx-controls:21.0.2")
-    implementation("org.openjfx:javafx-fxml:11.0.1")
-    implementation("org.openjfx:javafx-swing:21.0.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("org.mockito:mockito-core:5.11.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.apache.commons:commons-compress:1.26.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 group = "luna"
@@ -57,26 +53,14 @@ java {
 
 sourceSets {
     main {
-        java {
-            srcDir("src/main/java")
-        }
         withConvention(KotlinSourceSet::class) {
             kotlin.srcDirs("plugins")
         }
     }
-    test {
-        java {
-            srcDir("src/test/java")
-        }
-    }
-}
-
-javafx {
-    version = "11"
-    modules("javafx.controls", "javafx.fxml", "javafx.swing")
 }
 
 tasks.withType<JavaCompile> {
+
     options.compilerArgs = MutableList(1) { "-Xlint:unchecked" }
     options.encoding = "UTF-8"
 }
