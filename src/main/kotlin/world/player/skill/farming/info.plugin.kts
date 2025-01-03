32c3de0ce7ff70c9741f12b686980890d3e68068
lare96 <lare96@live.com>
2025-01-02 23:26:05 -0500

    Farming placeholder

---
+++ src/main/kotlin/world/player/skill/farming/info.plugin.kts
@@ -0,0 +1,13 @@
+package world.player.skill.farming
+
+import api.plugin.dsl.plugin
+
+plugin {
+    name = "Farming"
+    description =
+        """
+        TODO everything
+        """
+    version = "1.0"
+    authors += "lare96"
+}
