8efbce3e7a162832a7ad88c32cca171232428fe6
lare96 <lare96@live.com>
2025-01-02 23:31:58 -0500

    Agility placeholder

---
+++ src/main/kotlin/world/player/skill/agility/info.plugin.kts
@@ -0,0 +1,13 @@
+package world.player.skill.agility
+
+import api.plugin.dsl.plugin
+
+plugin {
+    name = "Agility"
+    description =
+        """
+        TODO everything
+        """
+    version = "1.0"
+    authors += "lare96"
+}
