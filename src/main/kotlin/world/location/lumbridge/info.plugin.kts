7ea20f14455bba29d9dc4720c0d867a8bbcef838
hydrozoa <hydrozoa.rs@gmail.com>
2025-01-04 19:27:54 +0100

    lumbridge

---
+++ src/main/kotlin/world/location/lumbridge/info.plugin.kts
@@ -0,0 +1,13 @@
+package world.location.lumbridge
+
+import api.plugin.dsl.plugin
+
+plugin {
+    name = "Lumbridge"
+    description =
+        """
+        A plugin that poplates Lumbridge with NPCs.
+        """
+    version = "1.0"
+    authors += "hydrozoa"
+}
