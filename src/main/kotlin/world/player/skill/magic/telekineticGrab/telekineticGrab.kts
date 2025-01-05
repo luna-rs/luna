3443cfdfae48d21c966f12e912c9e0ebcbc872a0
lare96 <lare96@live.com>
2025-01-05 16:58:52 -0500

    Telekinetic grab

--- src/main/kotlin/world/player/skill/magic/telekineticGrab/telekineticGrab.kts
+++ src/main/kotlin/world/player/skill/magic/telekineticGrab/telekineticGrab.kts
@@ -3,10 +3,7 @@ package world.player.skill.magic.telekineticGrab
 import api.predef.*
 import io.luna.game.event.impl.UseSpellEvent.MagicOnGroundItemEvent
 
+// Intercept event for telegrab spell.
 on(MagicOnGroundItemEvent::class)
-    .filter { spellId == -1 }
-    .then { }
-
-
-// TODO use raycast to determine if you can reach it? or what are the rules? maybe make raycast
-//  return a list of blocked objects then determine by type?
+    .filter { spellId == 1168 }
+    .then { plr.submitAction(TelekineticGrabAction(plr, targetItem)) }
