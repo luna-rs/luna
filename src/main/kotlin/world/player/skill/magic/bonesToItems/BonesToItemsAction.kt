ae7f886d795da7c2a3df06d9208fabb688ab2c44
lare96 <lare96@live.com>
2025-01-12 14:59:12 -0500

    Charge orb spells, bones to bananas and peaches

---
+++ src/main/kotlin/world/player/skill/magic/bonesToItems/BonesToItemsAction.kt
@@ -0,0 +1,52 @@
+package world.player.skill.magic.bonesToItems
+
+import api.attr.Attr
+import api.predef.ext.*
+import io.luna.game.action.QueuedAction
+import io.luna.game.model.mob.Player
+import io.luna.game.model.mob.block.Graphic
+import world.player.Animations
+import world.player.Sounds
+import world.player.skill.magic.Magic
+
+/**
+ * A [QueuedAction] that converts bones into either bananas or peaches.
+ */
+class BonesToItemsAction(plr: Player, val type: BonesToItemsType) :
+    QueuedAction<Player>(plr, plr.bonesToItemsDelay, 3) {
+
+    companion object {
+
+        /**
+         * The item id of bones.
+         */
+        val BONES = 526
+
+        /**
+         * The attribute representing the time source for an action.
+         */
+        val Player.bonesToItemsDelay by Attr.timeSource()
+    }
+
+    override fun execute() {
+        if (Magic.checkRequirements(mob, type.level, type.requirements)) {
+            val count = mob.inventory.computeAmountForId(BONES)
+            if (count == 0) {
+                // todo proper message
+                mob.sendMessage("You do not have any bones to convert.")
+                return
+            }
+            mob.lock()
+            mob.playSound(Sounds.BONES_TO_ITEMS)
+            world.scheduleOnce(1) {
+                val name = if (type == BonesToItemsType.BANANAS) "bananas" else "peaches"
+                mob.inventory.replaceAll(BONES, type.id)
+                mob.animation(Animations.BONES_TO_ITEMS)
+                mob.graphic(Graphic(141, 100))
+                // todo proper message
+                mob.sendMessage("You turn the bones in your inventory into $name.")
+                mob.unlock()
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/world/player/skill/magic/chargeOrb/ChargeOrbAction.kt
@@ -0,0 +1,47 @@
+package world.player.skill.magic.chargeOrb
+
+import api.attr.Attr
+import api.predef.*
+import api.predef.ext.*
+import io.luna.game.action.QueuedAction
+import io.luna.game.model.item.Item
+import io.luna.game.model.mob.Player
+import io.luna.game.model.mob.block.Graphic
+import io.luna.game.model.mob.inter.GameTabSet.TabIndex
+import world.player.Animations
+import world.player.skill.magic.Magic
+
+/**
+ * A [QueuedAction] handles charging orbs.
+ */
+class ChargeOrbAction(plr: Player, val type: ChargeOrbType) : QueuedAction<Player>(plr, plr.chargeOrbDelay, 5) {
+
+    companion object {
+
+        /**
+         * The unpowered orb item id.
+         */
+        const val UNPOWERED_ORB = 567
+
+        /**
+         * The time source attribute for charging orbs.
+         */
+        val Player.chargeOrbDelay by Attr.timeSource()
+    }
+
+    override fun execute() {
+        if (Magic.checkRequirements(mob, type.level, type.requirements)) {
+            mob.lock()
+            mob.playSound(type.sound)
+            world.scheduleOnce(1) {
+                mob.inventory.add(Item(type.chargedOrb))
+                mob.animation(Animations.CHARGE_ORB)
+                mob.graphic(Graphic(type.graphic, 100))
+                // todo proper message
+                mob.sendMessage("You create ${articleItemName(type.chargedOrb).toLowerCase()}.");
+                mob.tabs.show(TabIndex.MAGIC)
+                mob.unlock()
+            }
+        }
+    }
+}
