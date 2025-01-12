880ceceaebc4e124710c67a9d8f41ca0f04a053b
lare96 <lare96@live.com>
2025-01-12 03:14:08 -0500

    Change enchanting to use item slot

--- src/main/kotlin/world/player/skill/magic/enchantJewellery/EnchantJewelleryAction.kt
+++ src/main/kotlin/world/player/skill/magic/enchantJewellery/EnchantJewelleryAction.kt
@@ -3,15 +3,15 @@ package world.player.skill.magic.enchantJewellery
 import api.attr.Attr
 import api.predef.*
 import io.luna.game.action.QueuedAction
+import io.luna.game.model.item.Item
 import io.luna.game.model.mob.Player
 import io.luna.game.model.mob.inter.GameTabSet.TabIndex
-import io.luna.net.msg.out.SoundMessageWriter
 import world.player.skill.magic.Magic
 
 /**
  * A [QueuedAction] that handles enchanting rings, necklaces, and amulets.
  */
-class EnchantJewelleryAction(plr: Player, private val itemId: Int, private val type: EnchantJewelleryType) :
+class EnchantJewelleryAction(plr: Player, private val itemIndex: Int, private val type: EnchantJewelleryType) :
     QueuedAction<Player>(plr, plr.enchantJewelleryDelay, 3) {
 
     companion object {
@@ -24,23 +24,23 @@ class EnchantJewelleryAction(plr: Player, private val itemId: Int, private val t
 
     override fun execute() {
         if (Magic.checkRequirements(mob, type.level, type.requirements)) {
+            val itemId = mob.inventory.computeIdForIndex(itemIndex).orElse(-1)
             val enchantItem = type.enchantMap[itemId]
             if (enchantItem == null) {
                 // todo proper message
                 mob.sendMessage("You cannot use this spell on this item.")
                 return
             }
-            if (mob.inventory.replace(itemId, enchantItem.id)) {
-                mob.animation(enchantItem.animation)
-                mob.graphic(enchantItem.graphic)
-                mob.playSound(enchantItem.sound)
-                if (itemId == 1702) {
-                    mob.sendMessage("You successfully enchant the dragonstone amulet.");
-                } else {
-                    mob.sendMessage("You successfully enchant the ${itemName(itemId).toLowerCase()}.");
-                }
-                mob.tabs.show(TabIndex.MAGIC)
+            mob.inventory.set(itemIndex, Item(enchantItem.id))
+            mob.animation(enchantItem.animation)
+            mob.graphic(enchantItem.graphic)
+            mob.playSound(enchantItem.sound)
+            if (itemId == 1702) {
+                mob.sendMessage("You successfully enchant the dragonstone amulet.");
+            } else {
+                mob.sendMessage("You successfully enchant the ${itemName(itemId).toLowerCase()}.");
             }
+            mob.tabs.show(TabIndex.MAGIC)
         }
     }
 }
--- src/main/kotlin/world/player/skill/magic/enchantJewellery/enchantJewellery.kts
+++ src/main/kotlin/world/player/skill/magic/enchantJewellery/enchantJewellery.kts
@@ -6,5 +6,5 @@ import io.luna.game.event.impl.UseSpellEvent.MagicOnItemEvent
 /* Add all interactions for enchanting spells. */
 for (type in EnchantJewelleryType.ALL) {
     on(MagicOnItemEvent::class).filter { spellId == type.spellId }
-        .then { plr.submitAction(EnchantJewelleryAction(plr, targetItemId, type)) }
+        .then { plr.submitAction(EnchantJewelleryAction(plr, targetItemIndex, type)) }
 }
