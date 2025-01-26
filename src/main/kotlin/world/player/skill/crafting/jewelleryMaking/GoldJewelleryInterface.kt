9eeb5a9e37ae18447ce15ea5b094fdd024d9f33f
lare96 <lare96@live.com>
2025-01-25 20:03:54 -0500

    Gold jewellery making data and actions

--- src/main/kotlin/world/player/skill/crafting/jewelleryMaking/GoldJewelleryInterface.kt
+++ src/main/kotlin/world/player/skill/crafting/jewelleryMaking/GoldJewelleryInterface.kt
@@ -1,24 +1,60 @@
 package world.player.skill.crafting.jewelleryMaking
 
+import io.luna.game.model.item.IndexedItem
 import io.luna.game.model.mob.Player
 import io.luna.game.model.mob.inter.StandardInterface
+import io.luna.net.msg.out.ClearWidgetItemsMessageWriter
+import io.luna.net.msg.out.WidgetIndexedItemsMessageWriter
 import io.luna.net.msg.out.WidgetItemModelMessageWriter
+import io.luna.net.msg.out.WidgetTextMessageWriter
 
+/**
+ * A [StandardInterface] implementation that opens up the gold jewellery interface.
+ */
 class GoldJewelleryInterface : StandardInterface(4161) {
 
     override fun onOpen(plr: Player) {
-        /*
+        val hasRingMould = plr.inventory.contains(GoldJewelleryTable.RINGS.mouldId)
+        val hasNecklaceMould = plr.inventory.contains(GoldJewelleryTable.NECKLACES.mouldId)
+        val hasAmuletMould = plr.inventory.contains(GoldJewelleryTable.AMULETS.mouldId)
 
-		/* Rings */
-			{1635, 1637, 1639, 1641, 1643, 1645, 6575},
-		/* Neclece */
-			{1654, 1656, 1658, 1660, 1662, 1664, 6577},
-		/* amulet */
-			{1673, 1675, 1677, 1679, 1681, 1683, 6579}
+        if (hasRingMould) {
+            sendItems(plr, GoldJewelleryTable.RINGS)
+        } else {
+            resetItems(plr, GoldJewelleryTable.RINGS)
+        }
 
-        placeholders for all possible items
-         */
-        plr.queue(WidgetItemModelMessageWriter(4229, 100, 4151)) // works
-       // plr.queue(WidgetIndexedItemsMessageWriter(1635, IndexedItem(1, Item(4151, 1))))
+        if (hasNecklaceMould) {
+            sendItems(plr, GoldJewelleryTable.NECKLACES)
+        } else {
+            resetItems(plr, GoldJewelleryTable.NECKLACES)
+        }
+
+        if (hasAmuletMould) {
+            sendItems(plr, GoldJewelleryTable.AMULETS)
+        } else {
+            resetItems(plr, GoldJewelleryTable.AMULETS)
+        }
+    }
+
+    /**
+     * Sends a table of items on the interface.
+     */
+    private fun sendItems(plr: Player, table: GoldJewelleryTable) {
+        val interactableItems =
+            table.jewelleryItems.mapIndexed { index, jewellery -> IndexedItem(index, jewellery.item) }
+        plr.queue(WidgetIndexedItemsMessageWriter(table.buttonWidgetId, interactableItems))
+        plr.queue(WidgetTextMessageWriter("", table.textWidgetId))
+        plr.queue(WidgetItemModelMessageWriter(table.mouldWidgetId, 0, -1))
+    }
+
+    /**
+     * Resets a table of items on the interface.
+     */
+    private fun resetItems(plr: Player, table: GoldJewelleryTable) {
+        plr.queue(ClearWidgetItemsMessageWriter(table.buttonWidgetId))
+        plr.queue(WidgetTextMessageWriter("You need a ${table.baseName} mould to craft ${table.baseName}s.",
+                                          table.textWidgetId))
+        plr.queue(WidgetItemModelMessageWriter(table.mouldWidgetId, 100, table.mouldId))
     }
 }
