f46b9a2835be1d93aff042b66d8c79d1470881ea
lare96 <lare96@live.com>
2025-01-25 19:32:56 -0500

    Make signals bot extension functions for readability

--- src/main/kotlin/api/bot/scripts/GoHomeScript.kt
+++ src/main/kotlin/api/bot/scripts/GoHomeScript.kt
@@ -2,6 +2,7 @@ package api.bot.scripts
 
 import api.bot.CoroutineBotScript
 import api.bot.Signals
+import api.bot.Signals.within
 import api.bot.SuspendableFuture
 import io.luna.Luna
 import io.luna.game.model.Position
@@ -17,11 +18,11 @@ class GoHomeScript(bot: Bot) : CoroutineBotScript(bot) {
         // TODO Check if in wilderness, minigame, etc.
         val home = Luna.settings().game().startingPosition()
         if (bot.position.isWithinDistance(home, Region.SIZE)) {
-            botActions.walk(home).signalWhen(600, Signals.within(bot, home, 5)).await()
+            botActions.walk(home).signalWhen(600, bot.within(home, 5)).await()
         } else {
             val future = SuspendableFuture()
             botActions.sendCommand("home")
-            future.signalWhen(600, Signals.within(bot, home, 5)).await()
+            future.signalWhen(600, bot.within(home, 5)).await()
         }
     }
 }
