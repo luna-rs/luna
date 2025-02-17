c243028c54648d99bab6c35745eacb4b28964f73
lare96 <lare96@live.com>
2025-02-17 12:20:05 -0500

    Refactor events

--- src/main/java/io/luna/game/event/impl/WalkingEvent.java
+++ src/main/java/io/luna/game/event/impl/WalkingEvent.java
@@ -20,7 +20,15 @@ public final class WalkingEvent extends PlayerEvent implements ControllableEvent
      */
     private final boolean running;
 
+    /**
+     * The path size.
+     */
     private final int pathSize;
+
+    /**
+     * The opcode.
+     */
+    private final int opcode;
 
     /**
      * Creates a new {@link WalkingEvent}.
@@ -28,12 +36,15 @@ public final class WalkingEvent extends PlayerEvent implements ControllableEvent
      * @param player The player.
      * @param path The path that the player will walk.
      * @param running If the player is running.
+     * @param pathSize The path size.
+     * @param opcode The opcode.
      */
-    public WalkingEvent(Player player, Step[] path, boolean running, int pathSize) {
+    public WalkingEvent(Player player, Step[] path, boolean running, int pathSize, int opcode) {
         super(player);
         this.path = path;
         this.running = running;
         this.pathSize = pathSize;
+        this.opcode = opcode;
     }
 
     /**
@@ -50,7 +61,17 @@ public final class WalkingEvent extends PlayerEvent implements ControllableEvent
         return running;
     }
 
+    /**
+     * @return The path size.
+     */
     public int getPathSize() {
         return pathSize;
+    }
+
+    /**
+     * @return The opcode.
+     */
+    public int getOpcode() {
+        return opcode;
     }
 }
