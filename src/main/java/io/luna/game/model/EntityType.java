cf33078b6a524075c7a4887c4ddb83e0b15ca14a
lare96 <lare96@live.com>
2025-02-17 19:08:48 -0500

    Associate class type with entity type

--- src/main/java/io/luna/game/model/EntityType.java
+++ src/main/java/io/luna/game/model/EntityType.java
@@ -1,6 +1,11 @@
 package io.luna.game.model;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
+import io.luna.game.model.item.GroundItem;
+import io.luna.game.model.mob.Npc;
+import io.luna.game.model.mob.Player;
+import io.luna.game.model.object.GameObject;
 
 import java.util.EnumSet;
 import java.util.Set;
@@ -15,41 +20,74 @@ public enum EntityType {
     /**
      * A ground item.
      */
-    ITEM,
+    ITEM(GroundItem.class),
 
     /**
      * A game object.
      */
-    OBJECT,
+    OBJECT(GameObject.class),
 
     /**
      * A player mob.
      */
-    PLAYER,
+    PLAYER(Player.class),
 
     /**
      * A non-player mob.
      */
-    NPC,
+    NPC(Npc.class),
 
     /**
      * A local non-registered projectile.
      */
-    PROJECTILE,
+    PROJECTILE(LocalProjectile.class),
 
     /**
      * A local non-registered graphic.
      */
-    GRAPHIC,
+    GRAPHIC(LocalGraphic.class),
 
     /**
      * A local non-registered sound.
      */
-    SOUND;
+    SOUND(LocalSound.class);
 
     /**
      * An immutable enum set of these values.
      */
     public static final Set<EntityType> ALL = Sets.immutableEnumSet(EnumSet.allOf(EntityType.class));
 
+    /**
+     * An immutable map of class types to matching entity types.
+     */
+    public static final ImmutableMap<Class<?>, EntityType> CLASS_TO_TYPE;
+
+    static {
+        ImmutableMap.Builder<Class<?>, EntityType> builder = ImmutableMap.builder();
+        for (EntityType type : values()) {
+            builder.put(type.classType, type);
+        }
+        CLASS_TO_TYPE = builder.build();
+    }
+
+    /**
+     * The class type.
+     */
+    private final Class<?> classType;
+
+    /**
+     * Creates a new {@link EntityType}.
+     *
+     * @param type The class type.
+     */
+    EntityType(Class<?> type) {
+        this.classType = type;
+    }
+
+    /**
+     * @return The class type.
+     */
+    public Class<?> getClassType() {
+        return classType;
+    }
 }
