ee956188a348c9feebad752675b86e074cd48027
lare96 <lare96@live.com>
2025-05-17 11:55:05 -0400

    Following script changes

--- src/main/kotlin/world/player/following/followPlayer.kts
+++ src/main/kotlin/world/player/following/followPlayer.kts
@@ -3,6 +3,9 @@ package world.player.following
 import api.predef.*
 import io.luna.game.event.impl.PlayerClickEvent.PlayerThirdClickEvent
 
+/**
+ * Follows the target when the correct option is clicked.
+ */
 on(PlayerThirdClickEvent::class)
     .filter { plr.interactions.contains(INTERACTION_FOLLOW) }
-    .then { plr.submitAction(FollowingAction(plr, targetPlr)) }
+    .then { plr.follow(targetPlr) }
