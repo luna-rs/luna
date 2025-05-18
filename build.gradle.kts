dd19eddc9268513ac7fb18f91fe572787280401c
lare96 <lare96@live.com>
2025-05-18 00:24:31 -0400

    Refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.1")
     implementation("org.apache.logging.log4j:log4j-core:2.23.1")
     implementation("org.apache.logging.log4j:log4j-api:2.23.1")
-   implementation("org.slf4j:slf4j-nop:2.0.16")
+    implementation("org.slf4j:slf4j-nop:2.0.16")
     implementation("com.lmax:disruptor:3.4.2")
     implementation("io.netty:netty-all:4.1.107.Final")
     implementation("com.google.guava:guava:33.0.0-jre")
