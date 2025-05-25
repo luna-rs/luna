812c42aa580bae5827bd169f5b2db10be7aab27e
lare96 <lare96@live.com>
2025-05-25 01:03:55 -0400

    Refactoring

--- src/main/kotlin/world/player/skill/smithing/BarType.kt
+++ src/main/kotlin/world/player/skill/smithing/BarType.kt
@@ -9,27 +9,28 @@ import world.player.skill.smithing.smeltOre.SmeltAction
 /**
  * An enum representing a metal bar that can be made from a [SmeltAction].
  */
-enum class BarType(val id: Int, val level: Int, val xp: Double, val widget: Int, val oreRequired: List<Item>) {
+enum class BarType(val id: Int, val level: Int, val xp: Double, val widget: Int, val oreRequired: Pair<Item, Item?>) {
     BRONZE(id = 2349,
            level = 1,
            xp = 12.5,
            widget = 2405,
-           oreRequired = listOf(
-               item("Tin ore"),
+           oreRequired = Pair(
                item("Copper ore"),
+               item("Tin ore"),
            )),
     IRON(id = 2351,
          level = 15,
          xp = 25.0,
          widget = 2406,
-         oreRequired = listOf(
+         oreRequired = Pair(
              item("Iron ore"),
+             null
          )),
     STEEL(id = 2353,
           level = 30,
           xp = 37.5,
           widget = 2409,
-          oreRequired = listOf(
+          oreRequired = Pair(
               item("Iron ore"),
               item("Coal", 2),
           )),
@@ -37,21 +38,23 @@ enum class BarType(val id: Int, val level: Int, val xp: Double, val widget: Int,
            level = 20,
            xp = 13.7,
            widget = 2407,
-           oreRequired = listOf(
+           oreRequired = Pair(
                item("Silver ore"),
+               null
            )),
     GOLD(id = 2357,
          level = 40,
          xp = 22.5,
          widget = 2410,
-         oreRequired = listOf(
+         oreRequired = Pair(
              item("Gold ore"),
+             null
          )),
     MITHRIL(id = 2359,
             level = 50,
             xp = 50.0,
             widget = 2411,
-            oreRequired = listOf(
+            oreRequired = Pair(
                 item("Mithril ore"),
                 item("Coal", 4),
             )),
@@ -59,7 +62,7 @@ enum class BarType(val id: Int, val level: Int, val xp: Double, val widget: Int,
             level = 70,
             xp = 62.5,
             widget = 2412,
-            oreRequired = listOf(
+            oreRequired = Pair(
                 item("Adamantite ore"),
                 item("Coal", 6),
             )),
@@ -67,10 +70,20 @@ enum class BarType(val id: Int, val level: Int, val xp: Double, val widget: Int,
          level = 85,
          xp = 75.0,
          widget = 2413,
-         oreRequired = listOf(
+         oreRequired = Pair(
              item("Runite ore"),
              item("Coal", 8),
          ));
+
+    /**
+     * A list of all ore required to smelt this bar.
+     */
+    val oreList = oreRequired.toList().filterNotNull()
+
+    /**
+     * The lowercase name.
+     */
+    val lowercaseName = name.toLowerCase()
 
     companion object {
 
@@ -90,8 +103,10 @@ enum class BarType(val id: Int, val level: Int, val xp: Double, val widget: Int,
         val ORE_TO_BAR = values().run {
             val map = ImmutableSetMultimap.builder<Int, BarType>()
             for (bar in this) {
-                for (item in bar.oreRequired) {
-                    map.put(item.id, bar)
+                val oreRequired = bar.oreRequired
+                map.put(oreRequired.first.id, bar)
+                if(oreRequired.second != null) {
+                    map.put(oreRequired.second!!.id, bar)
                 }
             }
             return@run map.build()
