553c6a9ecb3eddba6d2864e3e974c7cac1a6996c
lare96 <lare96@live.com>
2025-06-02 10:06:42 -0400

    Add type adapter for ActiveSlayerTask

---
+++ src/main/kotlin/api/attr/typeAdapters/ActiveSlayerTaskTypeAdapter.kt
@@ -0,0 +1,39 @@
+package api.attr.typeAdapters
+
+import api.attr.Attr.readJsonMember
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
+import com.google.gson.stream.JsonWriter
+import io.luna.game.model.item.ItemContainer
+import io.luna.game.model.item.ItemContainer.StackPolicy
+import world.player.skill.slayer.ActiveSlayerTask
+import world.player.skill.slayer.SlayerMaster
+import world.player.skill.slayer.SlayerTaskType
+
+object ActiveSlayerTaskTypeAdapter : TypeAdapter<ActiveSlayerTask?>() { // todo documentation
+    override fun write(writer: JsonWriter, value: ActiveSlayerTask?) {
+        if(value != null) {
+            writer.beginObject()
+            writer.name("task").value(value.task.name)
+            writer.name("assignee").value(value.assignee.name)
+            writer.name("remaining").value(value.remaining)
+            writer.endObject()
+        } else {
+            writer.value("null")
+        }
+    }
+
+    override fun read(reader: JsonReader): ActiveSlayerTask? {
+        if(reader.peek() == JsonToken.STRING &&
+            reader.nextString().equals("null")) {
+            return null
+        }
+        reader.beginObject()
+        val task = readJsonMember(reader, "task") { SlayerTaskType.valueOf(it.nextString()) }
+        val assignee = readJsonMember(reader, "assignee") { SlayerMaster.valueOf(reader.nextString()) }
+        val remaining = readJsonMember(reader, "remaining") { it.nextInt() }
+        reader.endObject()
+        return ActiveSlayerTask(task, assignee, remaining)
+    }
+}
