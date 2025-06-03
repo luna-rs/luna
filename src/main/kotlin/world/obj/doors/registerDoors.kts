b63637f486159953c6bc3e13f2360422e20895dc
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-03 13:19:19 +0200

    Some doors now opening

--- src/main/kotlin/world/obj/doors/registerDoors.kts
+++ src/main/kotlin/world/obj/doors/registerDoors.kts
@@ -2,6 +2,7 @@ package world.obj.doors
 
 import api.predef.*
 import api.predef.ext.*
+import io.luna.game.model.`object`.*
 
 val doors: IntArray = intArrayOf(1533, 1534, 1519, 1516, 1530, 1531, 1536, )
 
@@ -9,5 +10,6 @@ doors.forEach {
     val doorID = it
     object1(doorID) {
         world.removeObject(gameObject.position, filter = {id == gameObject.id})
+        world.addObject(1534, gameObject.position, type = ObjectType.STRAIGHT_WALL, direction = ObjectDirection.SOUTH)
     }
 }
