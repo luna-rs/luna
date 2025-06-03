9d20cedf2c65a48b5d2f4046127846f533b25d9f
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-03 08:31:39 +0200

    Bankers facing east in Draynor village

--- src/main/kotlin/world/location/draynorVillage/draynorVillage.kts
+++ src/main/kotlin/world/location/draynorVillage/draynorVillage.kts
@@ -2,23 +2,15 @@ package world.location.draynorVillage
 
 import api.predef.*
 import api.predef.ext.*
-import api.shop.dsl.ShopHandler
 import io.luna.game.event.impl.ServerStateChangedEvent.ServerLaunchEvent
-import io.luna.game.model.item.shop.BuyPolicy
-import io.luna.game.model.item.shop.Currency
-import io.luna.game.model.item.shop.RestockPolicy
-import io.luna.game.model.mob.Player
-import io.luna.game.model.mob.Npc
-import io.luna.game.model.mob.Skill
-import io.luna.game.model.mob.block.UpdateFlagSet.UpdateFlag
-import io.luna.game.model.mob.dialogue.Expression
-import world.player.advanceLevel.LevelUpInterface
+import io.luna.game.model.*
+import java.util.*
 
 on(ServerLaunchEvent::class) {
     // Bankers
-    world.addNpc(494, 3090, 3245)
-    world.addNpc(494, 3090, 3243)
-    world.addNpc(494, 3090, 3242)
+    world.addNpc(494, 3090, 3245).defaultDirection = Optional.of(Direction.EAST)
+    world.addNpc(494, 3090, 3243).defaultDirection = Optional.of(Direction.EAST)
+    world.addNpc(494, 3090, 3242).defaultDirection = Optional.of(Direction.EAST)
 
     // Dark wizards
     world.addNpc(174, 3086, 3238)
