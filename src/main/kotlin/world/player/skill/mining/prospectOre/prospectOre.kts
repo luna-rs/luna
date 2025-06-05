732d4ee283774e72eec3e96adefed520d07b711e
lare96 <lare96@live.com>
2025-06-04 22:12:12 -0400

    Make ore prospecting a locked action

--- src/main/kotlin/world/player/skill/mining/prospectOre/prospectOre.kts
+++ src/main/kotlin/world/player/skill/mining/prospectOre/prospectOre.kts
@@ -2,36 +2,14 @@ package world.player.skill.mining.prospectOre
 
 import api.attr.Attr
 import api.predef.*
-import api.predef.ext.*
-import io.luna.game.action.impl.ThrottledAction
 import io.luna.game.model.mob.Player
-import world.player.Sounds
 import world.player.skill.mining.Ore
-
-/**
- * The mining ore prospectinmg timesource.
- */
-val Player.lastProspect by Attr.timeSource()
 
 /**
  * Prospect the ore, tells the player what it is.
  */
-fun prospect(plr: Player,  ore: Ore?) {
-    val delay = rand(2, 4);
-    plr.submitAction(object : ThrottledAction<Player>(plr, plr.lastProspect, delay + 1) {
-        override fun execute() {
-            plr.playSound(Sounds.PROSPECT_ORE)
-            plr.sendMessage("You examine the rock for ores...");
-            plr.walking.isLocked = true
-            world.scheduleOnce(rand(2, 4)) {
-                when (ore) { // todo sound 431
-                    null -> plr.sendMessage("There is no ore left in the rock.")
-                    else -> plr.sendMessage("This rock contains ${ore.typeName.toLowerCase()}.")
-                }
-                plr.walking.isLocked = false
-            }
-        }
-    })
+fun prospect(plr: Player, ore: Ore?) {
+    plr.submitAction(ProspectOreAction(plr, ore))
 }
 
 // Add listeners for all ores.
