992af5b8bed84e630d0e82e1f407783e541b73b5
hydrozoa <hydrozoa.rs@gmail.com>
2025-06-08 12:27:35 +0200

    more ImmutableList

--- src/main/kotlin/world/player/item/jewelleryTeleport/TeleportJewellery.kt
+++ src/main/kotlin/world/player/item/jewelleryTeleport/TeleportJewellery.kt
@@ -10,14 +10,14 @@ import io.luna.game.model.*
  * @param lastChargeMessage     chatbox message sent when the last charge is used
  * @param disappear             true if the item disappears on last charge use
  */
-enum class TeleportJewellery(val itemIDs: IntArray,
+enum class TeleportJewellery(val itemIDs: ImmutableList<Int>,
                              val destinations: ImmutableList<Pair<Position, String>>,
                              val rubMessage : String,
                              val lastChargeMessage : String,
                              val disappear: Boolean) {
 
     GAMES_NECKLACE(
-        intArrayOf(3853, 3855, 3857, 3859, 3861, 3863, 3865, 3867),
+        ImmutableList.of(3853, 3855, 3857, 3859, 3861, 3863, 3865, 3867),
         ImmutableList.of(
             Position(2897, 3554) to "Burthorpe.",
             Position(2536, 3565) to "Barbarian Outpost."),
@@ -26,7 +26,7 @@ enum class TeleportJewellery(val itemIDs: IntArray,
         true
     ),
     DUELING_RING(
-        intArrayOf(2552, 2554, 2556, 2558, 2560, 2562, 2564, 2566),
+        ImmutableList.of(2552, 2554, 2556, 2558, 2560, 2562, 2564, 2566),
         ImmutableList.of(
             Position(3316, 3237) to "Al Kharid Duel Arena.",
             Position(2441, 3088) to "Castle Wars Arena."),
@@ -35,7 +35,7 @@ enum class TeleportJewellery(val itemIDs: IntArray,
         true
     ),
     AMULET_OF_GLORY(
-        intArrayOf(1712, 1710, 1708, 1706, 1704),
+        ImmutableList.of(1712, 1710, 1708, 1706, 1704),
         ImmutableList.of(
             Position(3087, 3496) to "Edgeville.",
             Position(2918, 3176) to "Karamja.",
