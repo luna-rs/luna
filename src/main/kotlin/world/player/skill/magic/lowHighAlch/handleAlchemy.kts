4f6a40c8328f5482424e0b22a10d5000028bec3b
lare96 <lare96@live.com>
2025-06-08 22:16:17 -0400

    Refactoring

--- src/main/kotlin/world/player/skill/magic/lowHighAlch/handleAlchemy.kts
+++ src/main/kotlin/world/player/skill/magic/lowHighAlch/handleAlchemy.kts
@@ -3,7 +3,6 @@ package world.player.skill.magic.lowHighAlch
 import api.predef.*
 import io.luna.game.event.impl.UseSpellEvent.MagicOnItemEvent
 
-
 /* Intercept events for low and high alchemy. */
 on(MagicOnItemEvent::class).filter { spellId == 1162 }
     .then { plr.submitAction(AlchemyAction(plr, AlchemyType.LOW, targetItemIndex)) }
