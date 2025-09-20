7095517fcf751af83bdf978ae78242a09356d95d
lare96 <lare96@live.com>
2025-09-20 15:27:37 -0400

    Documentation

--- src/main/kotlin/api/attr/typeAdapters/AttributeMapTypeAdapter.kt
+++ src/main/kotlin/api/attr/typeAdapters/AttributeMapTypeAdapter.kt
@@ -6,7 +6,13 @@ import com.google.gson.stream.JsonWriter
 import io.luna.game.model.mob.attr.Attribute
 import io.luna.game.model.mob.attr.AttributeMap
 
+/**
+ * A type adapter that will enable [AttributeMap]s to be persisted as attributes.
+ *
+ * @author lare96
+ */
 object AttributeMapTypeAdapter : TypeAdapter<AttributeMap>() {
+
     override fun write(writer: JsonWriter, value: AttributeMap?) {
         val attributeList = if (value != null) value.save() else emptyList()
         writer.value(Attribute.getGsonInstance().toJson(attributeList))
