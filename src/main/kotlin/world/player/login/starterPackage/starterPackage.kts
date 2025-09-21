4264a9324815e6b480d5ec6ae4a7af59d0af94d8
lare96 <lare96@live.com>
2025-09-21 01:30:52 -0400

    Starter package now uses named items

--- src/main/kotlin/world/player/login/starterPackage/starterPackage.kts
+++ src/main/kotlin/world/player/login/starterPackage/starterPackage.kts
@@ -11,29 +11,29 @@ import world.player.login.firstLogin
  * Inventory starter items.
  */
 val inventoryStarter = listOf(
-    Item(995, 10000), // Coins
-    Item(556, 250), // Air runes
-    Item(555, 250), // Water runes
-    Item(554, 250), // Fire runes
-    Item(557, 250), // Earth runes
-    Item(558, 500), // Mind runes
-    Item(841)) // Shortbow
+    item("Coins", 10_000),
+    item("Air rune", 250),
+    item("Water rune", 250),
+    item("Fire rune", 250),
+    item("Earth rune", 250),
+    item("Mind rune", 500),
+    item("Shortbow"))
 
 /**
  * Equipment starter items.
  */
 val equipmentStarter = listOf(
-    Item(1153), // Iron full helm
-    Item(1115), // Iron platebody
-    Item(1067), // Iron platelegs
-    Item(1323), // Iron scimitar
-    Item(1191), // Iron kiteshield
-    Item(1731), // Amulet of power
-    Item(4121), // Iron boots
-    Item(1063), // Leather vambraces
-    Item(2570), // Ring of life
+    item("Iron full helm"),
+    item("Iron platebody"),
+    item("Iron platelegs"),
+    item("Iron scimitar"),
+    item("Iron kiteshield"),
+    item("Amulet of power"),
+    item("Iron boots"),
+    item("Leather vambraces"),
+    item("Ring of life"),
     Item(1019), // Black cape
-    Item(882, 750)) // Bronze arrows
+    item("Bronze arrow", 750))
 
 /**
  * Called when the player logs in for the first time.
