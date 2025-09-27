4fe8134182a636d7be35e90aa967164925eee941
lare96 <lare96@live.com>
2025-09-27 18:16:17 -0400

    NPC spawns from JSON file

---
+++ src/main/kotlin/world/npc/globalSpawn/NpcSpawnFileParser.kt
@@ -0,0 +1,44 @@
+package world.npc.globalSpawn
+
+import api.predef.*
+import api.predef.ext.*
+import com.google.common.collect.ImmutableList
+import com.google.gson.JsonObject
+import io.luna.game.model.Position
+import io.luna.game.model.def.NpcDefinition
+import io.luna.util.GsonUtils
+import io.luna.util.parser.JsonFileParser
+import java.nio.file.Paths
+import java.util.*
+
+/**
+ * Loads the global item spawn JSON file.
+ */
+internal class NpcSpawnFileParser : JsonFileParser<PersistentNpc>(PATH) {
+
+    companion object {
+
+        /**
+         * The path to the file.
+         */
+        private val PATH = Paths.get("data", "game", "world", "npcSpawns.json")
+    }
+
+    override fun convert(token: JsonObject): PersistentNpc {
+        val nameOrId = if (token.has("name")) token["name"].asString else token["id"].asInt
+        val position = GsonUtils.getAsType(token["position"], Position::class.java)
+        val id = if (nameOrId is Int) nameOrId else NpcDefinition.ALL.find { it.name == nameOrId }?.id
+        val respawn = if (token.has("respawn_ticks")) token["respawn_ticks"].asInt else 50
+        val wander = if (token.has("wander_radius")) token["wander_radius"].asInt else 0
+        if (id == null) {
+            throw IllegalStateException("No NPC found for ID/name [$nameOrId].")
+        }
+        return PersistentNpc(id, position, respawn, wander)
+    }
+
+    override fun onCompleted(tokenObjects: ImmutableList<PersistentNpc>) {
+        if (tokenObjects.isNotEmpty()) {
+            logger.debug("Loaded ${tokenObjects.size} global NPC spawns!")
+        }
+    }
+}
