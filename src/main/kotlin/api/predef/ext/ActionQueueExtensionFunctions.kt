9c6c633d95958901052bdc94673a22249967b5a9
lare96 <lare96@live.com>
2025-09-30 13:49:36 -0400

    Action queue ext

---
+++ src/main/kotlin/api/predef/ext/ActionQueueExtensionFunctions.kt
@@ -0,0 +1,10 @@
+package api.predef.ext
+
+import io.luna.game.action.Action
+import io.luna.game.action.ActionQueue
+import kotlin.reflect.KClass
+
+/**
+ * An implementation of [ActionQueue.contains] for [KClass].
+ */
+fun ActionQueue.contains(type: KClass<out Action<*>>) = contains(type.java)
