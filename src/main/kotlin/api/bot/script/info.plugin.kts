b5456815bb6b25bc1eb5faf6e040aaa2ad45332f
lare96 <lare96@live.com>
2025-10-04 19:54:03 -0400

    Bot script registration plugin

---
+++ src/main/kotlin/api/bot/script/info.plugin.kts
@@ -0,0 +1,13 @@
+package api.bot.script
+
+import api.plugin.dsl.plugin
+
+plugin {
+    name = "Bot scripts"
+    description =
+        """
+        A plugin that registers all bot scripts to be persisted.
+        """
+    version = "1.0"
+    authors += "lare96"
+}
---
+++ src/main/kotlin/api/bot/script/registerScripts.kts
@@ -0,0 +1,12 @@
+package api.bot.script
+
+import api.predef.*
+import io.luna.game.event.impl.ServerStateChangedEvent.ServerLaunchEvent
+
+/**
+ * Register all speech injectors.
+ */
+on(ServerLaunchEvent::class) {
+    val scriptManager = world.botManager.scriptManager
+    scriptManager.addScript(IdleBotScript::class) { bot, data -> IdleBotScript(bot, data) }
+}
