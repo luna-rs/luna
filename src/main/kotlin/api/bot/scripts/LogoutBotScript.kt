d64e700fb386dcc3ad74b8425abdb18faa82d13f
lare96 <lare96@live.com>
2025-10-07 01:40:49 -0400

    LogoutBotScript

---
+++ src/main/kotlin/api/bot/scripts/LogoutBotScript.kt
@@ -0,0 +1,29 @@
+package api.bot.scripts
+
+import api.bot.BotScript
+import api.bot.Suspendable.naturalDecisionDelay
+import io.luna.game.model.mob.bot.Bot
+import io.luna.game.model.mob.bot.script.BotScriptSnapshot
+
+/**
+ * A simple script that will make a [Bot] logout.
+ *
+ * @author lare96
+ */
+class LogoutBotScript(bot: Bot, private var urgent: Boolean) : BotScript<Boolean>(bot) {
+
+    override suspend fun run() {
+        if (urgent) {
+            // Simulate a bot x-logging/turning off their PC lol (dumb bots).
+            bot.client.isPendingLogout = true
+        } else {
+            bot.output.clickLogout()
+            bot.naturalDecisionDelay()
+        }
+    }
+
+    override fun snapshot(): Boolean = urgent
+    override fun load(snapshot: BotScriptSnapshot<Boolean>) {
+        urgent = snapshot.data
+    }
+}
