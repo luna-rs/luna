0bb4d9eadb76df2f2ee8d89de98ec1226cca3fa9
lare96 <lare96@live.com>
2025-10-18 19:21:19 -0400

    Add logic to privacy incoming packet

--- src/main/java/io/luna/net/msg/in/PrivacyOptionMessageReader.java
+++ src/main/java/io/luna/net/msg/in/PrivacyOptionMessageReader.java
@@ -1,7 +1,9 @@
 package io.luna.net.msg.in;
 
-import io.luna.game.event.impl.NullEvent;
+import io.luna.game.event.impl.PrivacyModeChangedEvent;
 import io.luna.game.model.mob.Player;
+import io.luna.game.model.mob.PlayerPrivacy;
+import io.luna.game.model.mob.PlayerPrivacy.PrivacyMode;
 import io.luna.net.msg.GameMessage;
 import io.luna.net.msg.GameMessageReader;
 
@@ -10,10 +12,19 @@ import io.luna.net.msg.GameMessageReader;
  *
  * @author lare96
  */
-public final class PrivacyOptionMessageReader extends GameMessageReader<NullEvent> {
+public final class PrivacyOptionMessageReader extends GameMessageReader<PrivacyModeChangedEvent> {
 
     @Override
-    public NullEvent decode(Player player, GameMessage msg) {
-        return NullEvent.INSTANCE;
+    public PrivacyModeChangedEvent decode(Player player, GameMessage msg) {
+        PrivacyMode publicChatMode = PrivacyMode.fromId(msg.getPayload().get());
+        PrivacyMode privateChatMode = PrivacyMode.fromId(msg.getPayload().get());
+        PrivacyMode tradeMode = PrivacyMode.fromId(msg.getPayload().get());
+        PlayerPrivacy newPrivacy = new PlayerPrivacy(publicChatMode, privateChatMode, tradeMode);
+        return new PrivacyModeChangedEvent(player, player.getPrivacyOptions(), newPrivacy);
+    }
+
+    @Override
+    public void handle(Player player, PrivacyModeChangedEvent event) {
+        player.setPrivacyOptions(event.getNewPrivacy());
     }
 }
