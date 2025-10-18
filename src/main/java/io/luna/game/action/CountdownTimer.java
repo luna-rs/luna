c0e533a9301264065b67776537178772cafb4ab7
lare96 <lare96@live.com>
2025-10-18 19:20:11 -0400

    Delete username cache

---
+++ src/main/java/io/luna/game/action/CountdownTimer.java
@@ -0,0 +1,53 @@
+package io.luna.game.action;
+
+import io.luna.game.model.mob.Mob;
+
+public  class CountdownTimer extends Action<Mob> {
+
+    private long start;
+    private long remaining;
+
+    public CountdownTimer(Mob mob, long start) {
+        super(mob, ActionType.SOFT);
+        this.start = start;
+        remaining = start;
+    }
+
+    public void onComplete() {
+
+    }
+
+    public void onCountdown() {
+
+    }
+
+    @Override
+    public boolean run() {
+        long result = --remaining;
+        if (result <= 0) {
+            onComplete();
+            return true;
+        }
+        onCountdown();
+        return false;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+        if(start < remaining) {
+            remaining = start;
+        }
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setRemaining(long remaining) {
+        this.remaining = remaining;
+    }
+
+    public long getRemaining() {
+        return remaining;
+    }
+}
