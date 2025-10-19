2b964a0ed926bbc890fa16d9f96ebb9cdfcaf982
lare96 <lare96@live.com>
2025-10-18 23:31:25 -0400

    Remove combat code, add code to Controllers.kt

--- src/main/kotlin/api/controller/Controllers.kt
+++ src/main/kotlin/api/controller/Controllers.kt
@@ -1,9 +1,12 @@
 package api.controller
 
+import io.luna.game.model.mob.Mob
+import io.luna.game.model.mob.Npc
 import io.luna.game.model.mob.controller.ControllerKey
 import io.luna.game.model.mob.controller.PlayerController
 import world.player.combat.MultiCombatAreaController
 import world.player.wilderness.WildernessAreaController
+import world.player.wilderness.WildernessAreaController.wildernessLevel
 import kotlin.reflect.KClass
 
 /* All controller properties go here. */
@@ -17,3 +20,20 @@ object Controllers {
  */
 fun <T : PlayerController> of(controllerType: KClass<T>, controllerSupplier: () -> T) =
     ControllerKey.of(controllerType.java, controllerSupplier)
+
+/**
+ * Determines if a [Mob] has a [MultiCombatAreaController] registered.
+ */
+fun Mob.inMultiArea(): Boolean {
+    return if(this is Npc) MultiCombatAreaController.inside(position) else
+        asPlr().controllers.contains(Controllers.MULTICOMBAT)
+}
+
+/**
+ * Determines if a [Mob] has a [WildernessAreaController] registered.
+ */
+fun Mob.inWilderness(): Boolean {
+    return if(this is Npc) WildernessAreaController.inside(position) else
+        asPlr().wildernessLevel > 0
+
+}
