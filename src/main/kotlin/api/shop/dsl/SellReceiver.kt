9a9d6153ed5cb315b1ce1cfda08ace4ad87323b9
lare96 <lare96@live.com>
2025-10-18 23:29:30 -0400

    Fix to the shop sell receiver for noted items

--- src/main/kotlin/api/shop/dsl/SellReceiver.kt
+++ src/main/kotlin/api/shop/dsl/SellReceiver.kt
@@ -1,6 +1,6 @@
 package api.shop.dsl
 
-import io.luna.game.model.def.ItemDefinition
+import io.luna.game.model.item.Item
 
 /**
  * The receiver for the [ShopReceiver.sell] closure.
@@ -10,7 +10,7 @@ import io.luna.game.model.def.ItemDefinition
 class SellReceiver(private val shop: ShopReceiver, private val noted: Boolean) {
 
     /**
-     * Returns an instance of this receiver that works with noted items.
+     * Returns a new [SellReceiver] instance that works with noted items.
      */
     fun noted(func: SellReceiver.() -> Unit) = func(SellReceiver(shop, true))
 
@@ -18,11 +18,7 @@ class SellReceiver(private val shop: ShopReceiver, private val noted: Boolean) {
      * Adds an item to the shop.
      */
     infix fun String.x(amount: Int) {
-        val id = ItemDefinition.ALL
-            .lookup { it.isTradeable && it.name == this && it.isNoted == noted }
-            .map { it.id }
-            .orElseThrow { NoSuchElementException("Item ($this) in shop (${shop.name}) was not valid or found.") }
-        id.x(amount)
+        Item.findId(this, noted).x(amount)
     }
 
     /**
