2a3dae9502c0e2596f1f0c8a63b73148aa10c1d1
lare96 <lare96@live.com>
2025-10-25 23:01:59 -0400

    ExecutorUtils cleanup

--- src/main/java/io/luna/util/ExecutorUtils.java
+++ src/main/java/io/luna/util/ExecutorUtils.java
@@ -3,12 +3,9 @@ package io.luna.util;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 import java.util.concurrent.TimeUnit;
@@ -21,11 +18,6 @@ import java.util.concurrent.TimeUnit;
 public final class ExecutorUtils {
 
     /**
-     * The asynchronous logger.
-     */
-    private static final Logger logger = LogManager.getLogger();
-
-    /**
      * Create a new thread pool with {@code threads} workers.
      *
      * @param name The naming scheme for the workers in the pool.
@@ -33,10 +25,6 @@ public final class ExecutorUtils {
      * @return The thread pool.
      */
     public static ListeningExecutorService threadPool(String name, int threads) {
-        int maxRecommendedThreads = ThreadUtils.cpuCount() * 2;
-        if (threads > maxRecommendedThreads) {
-            logger.warn("Exceeding maximum number of recommended threads for cached thread pool (threads: {}).", threads);
-        }
         var threadPool = new ThreadPoolExecutor(threads, threads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
         var threadFactory = new ThreadFactoryBuilder().setNameFormat(name).build();
         threadPool.setThreadFactory(threadFactory);
@@ -51,27 +39,7 @@ public final class ExecutorUtils {
      * @return The new cached thread pool.
      */
     public static ListeningExecutorService threadPool(String name) {
-        return threadPool(name, ThreadUtils.cpuCount());
-    }
-
-    /**
-     * Generates a {@link ThreadFactory} for {@code classType}.
-     *
-     * @param classType The class type.
-     * @return The generated thread factory.
-     */
-    public static ThreadFactory threadFactory(Class<?> classType) {
-        return threadFactory(classType.getSimpleName() + "Thread");
-    }
-
-    /**
-     * Generates a {@link ThreadFactory} with {@code nameFormat}.
-     *
-     * @param nameFormat The name of the threads.
-     * @return The generated thread factory.
-     */
-    public static ThreadFactory threadFactory(String nameFormat) {
-        return new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
+        return threadPool(name, Runtime.getRuntime().availableProcessors());
     }
 
     /**
