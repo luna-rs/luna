b7d82a47b6c510a82f18c2012125861413fd3a0c
lare96 <lare96@live.com>
2025-11-01 00:59:04 -0400

    BotData impl of PlayerData

---
+++ src/main/java/io/luna/game/persistence/BotData.java
@@ -0,0 +1,44 @@
+package io.luna.game.persistence;
+
+import io.luna.game.GameService;
+import io.luna.game.LogoutService;
+import io.luna.game.model.mob.Player;
+import io.luna.game.model.mob.bot.Bot;
+import io.luna.game.model.mob.bot.script.BotScriptSnapshot;
+
+import java.util.List;
+
+/**
+ * A model acting as a proxy for {@link Bot} save data. It primarily ensures thread safety for interactions between
+ * {@link GameService} and {@link LogoutService}. All fields in this class intended for serialization should be public.
+ *
+ * @author lare96
+ */
+public final class BotData extends PlayerData {
+
+    public List<BotScriptSnapshot<?>> scripts;
+
+    /**
+     * Creates a new {@link BotData}.
+     *
+     * @param username The username of the bot this data belongs to.
+     */
+    public BotData(String username) {
+        super(username);
+    }
+
+    @Override
+    public void load(Player player) {
+        super.load(player);
+        Bot bot = (Bot) player;
+        bot.getScriptStack().load(scripts);
+    }
+
+    @Override
+    public PlayerData save(Player player) {
+        super.save(player);
+        Bot bot = (Bot) player;
+        scripts = bot.getScriptStack().save();
+        return this;
+    }
+}
