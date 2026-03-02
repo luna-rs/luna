bcd8c99a4990b24647d2fdf28f0b51fc14159241
lare96 <lare96@live.com>
2026-03-02 02:05:17 -0500

    Fix AttributeTest

--- src/test/java/io/luna/game/model/mob/attr/AttributeTest.java
+++ src/test/java/io/luna/game/model/mob/attr/AttributeTest.java
@@ -1,6 +1,5 @@
 package io.luna.game.model.mob.attr;
 
-import io.luna.util.GsonUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 /**
  * Unit tests for {@link Attribute}.
  *
- * @author lare96 
+ * @author lare96
  */
 final class AttributeTest {
 
@@ -20,19 +19,17 @@ final class AttributeTest {
 
     @Test
     void illegalKeys() {
-        var gson = GsonUtils.GSON;
-
         // Test for duplicate keys.
-        new Attribute<>(() -> false).persist("duplicate_key");
-        assertThrows(IllegalStateException.class, () -> new Attribute<>(() -> false).persist("duplicate_key"));
+        new Attribute<>(Boolean.class, () -> false).persist("duplicate_key");
+        assertThrows(IllegalStateException.class, () -> new Attribute<>(Boolean.class, () -> false).persist("duplicate_key"));
 
         // Test for empty key.
-        assertThrows(IllegalArgumentException.class, () -> new Attribute<>(() -> false).persist(""));
+        assertThrows(IllegalArgumentException.class, () -> new Attribute<>(Boolean.class, () -> false).persist(""));
 
         // Test for whitespace characters.
-        assertThrows(IllegalArgumentException.class, () -> new Attribute<>(() -> false).persist("test key"));
+        assertThrows(IllegalArgumentException.class, () -> new Attribute<>(Boolean.class, () -> false).persist("test key"));
 
         // Test for uppercase characters.
-        assertThrows(IllegalArgumentException.class, () -> new Attribute<>(() -> false).persist("TEST_KEY"));
+        assertThrows(IllegalArgumentException.class, () -> new Attribute<>(Boolean.class, () -> false).persist("TEST_KEY"));
     }
 }
