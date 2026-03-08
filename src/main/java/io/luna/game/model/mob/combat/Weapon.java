e4112b7688156f2d1ac83d8f50f72401520da8c5
lare96 <lare96@live.com>
2026-03-08 03:51:54 -0400

    Weapon enum

---
+++ src/main/java/io/luna/game/model/mob/combat/Weapon.java
@@ -0,0 +1,132 @@
+package io.luna.game.model.mob.combat;
+
+/**
+ * The supported weapon interface categories used by the combat system.
+ * <p>
+ * Each weapon type represents a combat tab grouping that determines the available {@link CombatStyle} options,
+ * interface layout, and related combat behavior for an equipped weapon.
+ *
+ * @author lare96
+ */
+public enum Weapon {
+
+    /**
+     * The staff weapon type.
+     */
+    STAFF,
+
+    /**
+     * The warhammer weapon type.
+     */
+    WARHAMMER,
+
+    /**
+     * The scythe weapon type.
+     */
+    SCYTHE,
+
+    /**
+     * The battleaxe weapon type.
+     */
+    BATTLEAXE,
+
+    /**
+     * The crossbow weapon type.
+     */
+    CROSSBOW,
+
+    /**
+     * The shortbow weapon type.
+     */
+    SHORTBOW,
+
+    /**
+     * The longbow weapon type.
+     */
+    LONGBOW,
+
+    /**
+     * The dagger weapon type.
+     */
+    DAGGER,
+
+    /**
+     * The sword weapon type.
+     */
+    SWORD,
+
+    /**
+     * The scimitar weapon type.
+     */
+    SCIMITAR,
+
+    /**
+     * The sickle weapon type.
+     */
+    SICKLE,
+
+    /**
+     * The longsword weapon type.
+     */
+    LONGSWORD,
+
+    /**
+     * The mace weapon type.
+     */
+    MACE,
+
+    /**
+     * The knife weapon type.
+     */
+    KNIFE,
+
+    /**
+     * The spear weapon type.
+     */
+    SPEAR,
+
+    /**
+     * The two-handed sword weapon type.
+     */
+    TWO_HANDED_SWORD,
+
+    /**
+     * The pickaxe weapon type.
+     */
+    PICKAXE,
+
+    /**
+     * The claws weapon type.
+     */
+    CLAWS,
+
+    /**
+     * The halberd weapon type.
+     */
+    HALBERD,
+
+    /**
+     * The unarmed weapon type.
+     */
+    UNARMED,
+
+    /**
+     * The whip weapon type.
+     */
+    WHIP,
+
+    /**
+     * The thrown axe weapon type.
+     */
+    THROWN_AXE,
+
+    /**
+     * The dart weapon type.
+     */
+    DART,
+
+    /**
+     * The javelin weapon type.
+     */
+    JAVELIN
+}
