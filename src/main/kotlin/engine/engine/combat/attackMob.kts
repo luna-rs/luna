01325c6306cc827ea44ef9fa022ac818684bf032
lare96 <lare96@live.com>
2026-03-08 03:02:37 -0400

    Attack context menu listeners for npcs/players, magic on npc/player listeners

---
+++ src/main/kotlin/engine/engine/combat/attackMob.kts
@@ -0,0 +1,42 @@
+package engine.combat
+
+import api.predef.*
+import io.luna.game.event.EventPriority
+import io.luna.game.event.impl.NpcClickEvent.AttackNpcEvent
+import io.luna.game.event.impl.PlayerClickEvent.PlayerFirstClickEvent
+import io.luna.game.event.impl.UseSpellEvent.MagicOnNpcEvent
+import io.luna.game.event.impl.UseSpellEvent.MagicOnPlayerEvent
+
+
+// "Attack" context menu option on players.
+on(PlayerFirstClickEvent::class,
+   EventPriority.HIGH) {
+    if (plr.contextMenu.contains(OPTION_ATTACK) && targetPlr.hitpoints.level > 0) {
+        plr.combat.attack(targetPlr)
+    }
+}
+
+// Use magic spell on player.
+on(MagicOnPlayerEvent::class,
+   EventPriority.HIGH) {
+    if (plr.contextMenu.contains(OPTION_ATTACK) && targetPlr.hitpoints.level > 0) {
+        // start combat
+    }
+}
+
+// "Attack" context menu option on npcs.
+on(AttackNpcEvent::class,
+   EventPriority.HIGH) {
+    val def = targetNpc.definition
+    if (def.combatLevel > 0 && def.actions.contains("Attack")) {
+        plr.combat.attack(targetNpc)
+    }
+}
+
+// Use magic spell on NPC.
+on(MagicOnNpcEvent::class) {
+    val def = targetNpc.definition
+    if (def.combatLevel > 0 && def.actions.contains("Attack")) {
+        // start combat
+    }
+}
