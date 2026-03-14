9b158c2ef7c50ca26bba6b0e92b145115d18ee60
lare96 <lare96@live.com>
2026-03-14 06:46:52 -0400

    Fix #439

--- src/main/kotlin/engine/engine/combat/death/npcDeath.kts
+++ src/main/kotlin/engine/engine/combat/death/npcDeath.kts
@@ -2,7 +2,9 @@ package engine.combat.death
 
 import api.combat.death.DeathHookHandler
 import api.predef.*
+import game.skill.slayer.Slayer
 import io.luna.game.model.mob.Npc
+import io.luna.game.model.mob.Player
 import io.luna.game.model.mob.block.Animation
 import io.luna.game.model.mob.block.Animation.AnimationPriority
 
@@ -11,10 +13,13 @@ import io.luna.game.model.mob.block.Animation.AnimationPriority
  */
 DeathHookHandler.setDefaultHook(Npc::class) {
     preDeath {
-        victim.combatDef.ifPresent { victim.animation(Animation(it.deathAnimation, AnimationPriority.HIGH)) }
+        victim.animation(Animation(victim.combatDef.deathAnimation, AnimationPriority.IMMUTABLE))
     }
 
     death {
+        if(source is Player) {
+            Slayer.record(source, victim.id)
+        }
         drop()
         world.npcs.remove(victim)
     }
