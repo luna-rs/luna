48183f1c5a37890c913aa87602025ae537e7e86f
CJ <lare96@live.com>
2026-04-08 17:53:57 -0400

    Rename activate to activation in dragonAxe.kts

--- src/main/kotlin/game/game/combat/specialAttacks/dragonAxe.kts
+++ src/main/kotlin/game/game/combat/specialAttacks/dragonAxe.kts
@@ -1,13 +1,13 @@
 package game.combat.specialAttacks
 
-import api.combat.specialAttack.SpecialAttackHandler.activate
+import api.combat.specialAttack.SpecialAttackHandler.activation
 import api.predef.*
 import io.luna.game.model.mob.block.*
 import io.luna.game.model.mob.block.Animation.AnimationPriority
 import io.luna.game.model.mob.combat.SpecialAttackType.DRAGON_AXE
 
 // Lumber up!
-activate(type = DRAGON_AXE, drain = 100) {
+activation(type = DRAGON_AXE, drain = 100) {
     animation(Animation(2876, AnimationPriority.HIGH))
     graphic(Graphic(479, 120, 0))
     speak("Chop chop!")
