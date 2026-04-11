8e8f5d59ebfabb05225e383fb1a25bc0a9dbbb9b
lare96 <lare96@live.com>
2026-04-11 04:03:06 -0400

    Infernal mage npc hook

---
+++ src/main/kotlin/game/game/combat/npcHooks/infernalMage.kts
@@ -0,0 +1,7 @@
+import api.combat.npc.NpcCombatHandler.combat
+import io.luna.game.model.mob.combat.CombatSpell
+
+// Level 66 infernal mages.
+combat(1643, 1644, 1645, 1646, 1647) {
+    attack { magic(CombatSpell.FIRE_BLAST) }
+}
