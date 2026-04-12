4a3c2e36a9614c56506a36cc74bf95a26190530c
hydrozoa <hydrozoa.rs@gmail.com>
2026-04-12 17:29:47 +0200

    Added barbarians

---
+++ src/main/kotlin/game/game/combat/npcHooks/barbarian.kts
@@ -0,0 +1,17 @@
+package game.combat.npcHooks
+
+import api.combat.npc.NpcCombatHandler.combat
+import api.predef.*
+
+// Barbarians in Barbarian Village
+combat(3246, 3247, 3248, 3249, 3250, 3251, 3252, 3253, 3255, 3256, 3257, 3258, 3259, 3260, 3261, 3262, 3263) {
+    attack {
+        if (npc.combat.isAttackReady) {
+            val rand = rand(36)
+            if (rand < 8) {
+                npc.speak("YYEEEEEAAARRRRGGHHHH!!!");
+            }
+        }
+        melee()
+    }
+}
