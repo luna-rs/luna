bd5e870d3cd9fed28c0923289d17ed459097e55e
lare96 <lare96@live.com>
2026-04-12 23:24:38 -0400

    Fix basiliskAndCockatrice.kts

--- src/main/kotlin/game/game/combat/npcHooks/basiliskAndCockatrice.kts
+++ src/main/kotlin/game/game/combat/npcHooks/basiliskAndCockatrice.kts
@@ -4,6 +4,8 @@ import api.combat.npc.NpcCombatHandler.combat
 import api.predef.*
 import io.luna.Luna
 import io.luna.game.model.mob.Player
+import io.luna.game.model.mob.combat.damage.CombatDamageRequest
+import io.luna.game.model.mob.combat.damage.CombatDamageType
 import io.luna.util.RandomUtils
 
 if (Luna.settings().skills().slayerEquipmentNeeded()) {
@@ -28,30 +30,38 @@ if (Luna.settings().skills().slayerEquipmentNeeded()) {
          * Handles basilisk and cockatrice attack effects.
          *
          * When a player is attacked without a mirror shield equipped, the NPC has a chance to apply stat drains to one
-         * or more combat skills and notify the player that they have been weakened.
+         * or more combat skills, notify the player that they have been weakened, and deal a much more accurate attack.
          */
         combat(id) {
             attack {
                 melee {
-                    if (other is Player && other.equipment.shield?.id != MIRROR_SHIELD && RandomUtils.random()) {
+                    if (other is Player && other.equipment.shield?.id != MIRROR_SHIELD) {
                         if (RandomUtils.random()) {
-                            other.attack.adjustLevel(-rand(10, 20))
+                            if (RandomUtils.random()) {
+                                other.attack.adjustLevel(-rand(10, 20))
+                            }
+                            if (RandomUtils.random()) {
+                                other.strength.adjustLevel(-rand(10, 20))
+                            }
+                            if (RandomUtils.random()) {
+                                other.defence.adjustLevel(-rand(10, 20))
+                            }
+                            if (RandomUtils.random()) {
+                                other.ranged.adjustLevel(-rand(10, 20))
+                            }
+                            if (RandomUtils.random()) {
+                                other.magic.adjustLevel(-rand(10, 20))
+                            }
+                            other.sendMessage("You have been weakened.")
                         }
-                        if (RandomUtils.random()) {
-                            other.strength.adjustLevel(-rand(10, 20))
-                        }
-                        if (RandomUtils.random()) {
-                            other.defence.adjustLevel(-rand(10, 20))
-                        }
-                        if (RandomUtils.random()) {
-                            other.ranged.adjustLevel(-rand(10, 20))
-                        }
-                        if (RandomUtils.random()) {
-                            other.magic.adjustLevel(-rand(10, 20))
-                        }
-                        other.sendMessage("You have been weakened.")
+                        CombatDamageRequest.Builder(attacker, victim, CombatDamageType.MELEE)
+                            .setBaseMaxHit(11)
+                            .setBaseAccuracy(0.9)
+                            .build().resolve()
+                    } else {
+                        it
                     }
-                    it
+
                 }
             }
         }
