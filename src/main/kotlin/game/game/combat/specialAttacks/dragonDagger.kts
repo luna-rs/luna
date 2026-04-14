3c1d3bb1c8f2bd3fe489465d966f844261825ce0
lare96 <lare96@live.com>
2026-04-14 18:38:16 -0400

    Update docs

--- src/main/kotlin/game/game/combat/specialAttacks/dragonDagger.kts
+++ src/main/kotlin/game/game/combat/specialAttacks/dragonDagger.kts
@@ -16,26 +16,16 @@ val ANIMATION = 1062
  */
 val GRAPHIC = Graphic(252, 100, 0)
 
-/*
- * Registers the dragon dagger special attack.
- *
- * This special attack drains 25% special attack energy, uses a 25% accuracy bonus, and applies a 15% damage bonus
- * to the manually resolved hit performed during the launch stage.
- */
 attack(type = DRAGON_DAGGER,
        drain = 25,
        attackBonus = 0.25,
        damageBonus = 0.15) {
 
-    // Starts the special attack using the dragon dagger special attack animation.
+    // Use the dragon dagger special attack animation.
     attack { melee(ANIMATION) }
 
-    /*
-     * Resolves and applies the second strike.
-     *
-     * The hit is resolved as melee damage using the configured special attack accuracy and damage bonuses, then
-     * applied directly to the victim.
-     */
+
+    // Resolve and apply the second strike, apply graphics.
     launched {
         attacker.graphic(GRAPHIC)
 
