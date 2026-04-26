171ce2b6122470c9b84eed4c9d3c5a602208ff61
lare96 <lare96@live.com>
2026-04-25 22:21:36 -0400

    Fix errors

--- src/main/kotlin/engine/engine/interaction/follow/MobFollowAction.kt
+++ src/main/kotlin/engine/engine/interaction/follow/MobFollowAction.kt
@@ -12,6 +12,13 @@ import io.luna.game.model.mob.Mob
  */
 class MobFollowAction(mob: Mob, private val target: Mob) : Action<Mob>(mob, ActionType.WEAK) {
 
+    // todo use this in combat for player following instead of whatever you have now
+    // todo different types of following. what you have now is escort based following (walk behind)
+    // todo need a combat based following where direction doesn't matter (based on optional.empty)
+    // todo need line-of-sight as well
+    // todo this should also be managed through the navigator
+
+    // todo can probably be removed now?
     /**
      * The last position of this mob.
      */
@@ -29,7 +36,6 @@ class MobFollowAction(mob: Mob, private val target: Mob) : Action<Mob>(mob, Acti
         if (mob.position == target.position) {
             mob.navigator.stepRandom(false)
         } else {
-            mob.navigator.walkBehind(target, false)
             lastPosition = target.position
         }
         return false
