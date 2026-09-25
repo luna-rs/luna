34841e2f8080393ca82d1d6b85b090df4b8e3ef2
TheLining <pkdycwg4gj@privaterelay.appleid.com>
2026-09-25 16:35:57 -0400

    Fix bot gear loss and equipment requirement checks (#520)
    * Prevent bot gear loss during equipment changes
    
    * Fix bot equipment requirement check
    
    * Fix indentation in BotEquipmentActionHandler.kt
    
    * Add TODO for inventory space handling in unequip
    
    Added a TODO comment to handle inventory space issues during unequip.
    
    * Simplify unequip logic in BotGearLocator
    
    Removed inventory space check before unequipping items. The equipment is already checked before the loop
    
    ---------
    
    Co-authored-by: TheLining <333124608+TheLining@users.noreply.github.com>
    Co-authored-by: CJ <lare96@live.com>

--- src/main/kotlin/api/api/bot/action/BotEquipmentActionHandler.kt
+++ src/main/kotlin/api/api/bot/action/BotEquipmentActionHandler.kt
@@ -43,7 +43,7 @@ class BotEquipmentActionHandler(private val bot: Bot, private val handler: BotAc
             // Invalid item.
             bot.log("${itemName(id)} cannot be equipped.")
             return false
-        } else if(equipmentDef.map { it.meetsAllRequirements(bot) }.isEmpty) {
+        } else if (!equipmentDef.get().meetsAllRequirements(bot)) {
             bot.log("Does not meet equipment requirements.")
             return false
         }
